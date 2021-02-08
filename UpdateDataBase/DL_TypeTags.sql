749bbb5808621b2672ae6a3bed0e46c530b0ee8f
xiaoyu <3334559688@qq.com>
2021-02-08 23:07:21 +0800

    dacaogu

---
+++ UpdateDataBase/DL_TypeTags.sql
@@ -0,0 +1,13 @@
+--TypeTags
+insert or replace into TypeTags
+	(Type,									Tag)
+values
+	('UNIT_HUNGARY_BLACK_ARMY',				'CLASS_DACAOGU'),
+	('UNIT_HORSEMAN',						'CLASS_DACAOGU'),
+	('UNIT_ETHIOPIAN_OROMO_CAVALRY',		'CLASS_DACAOGU'),
+	('UNIT_COURSER',						'CLASS_DACAOGU');
+
+insert or replace into Tags
+	(Tag,				Vocabulary)
+values
+	('CLASS_DACAOGU',	'ABILITY_CLASS');
