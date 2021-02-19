9ab12bea6205baf6cc69c8964408534811ba3464
wltk <>
2021-02-18 22:28:13 -0800

    Support Suk's Simple UI mod with City Policies CityPanel
    1. Refactor CityPanelOverview_CityPolicies to call Base function instead
       of copy original code, which scales better. Achieved by modifying
       input data and passing on.
    2. Add support for Suk's Simple UI, unfortunately have to copy part of their code
       for now, since their CityPanelOverview file hasn't been imported into
       the game, so we won't be able to include it.
    3. Fix Amenity panel to include amenity from Governors, which is
       introduced in CityPanelOverview_Expansion1.
    4. Fix TradingPostsHeader's offset so now the text is centered.

--- CityPolicies/UI/CityPanelOverview_CityPolicies.lua
+++ CityPolicies/UI/CityPanelOverview_CityPolicies.lua
@@ -1,128 +1,76 @@
-include( "CityPanelOverview_Expansion2" )
+local tCityBannerVersions = {
+    "CityPanelOverview_Expansion2.lua",
+    "CityPanelOverview_Expansion1.lua",
+    "CityPanelOverview.lua",
+}
 
-local m_kBuildingsIM		:table = InstanceManager:new( "BuildingInstance",			"Top");
-local m_kDistrictsIM		:table = InstanceManager:new( "DistrictInstance",			"Top", Controls.BuildingAndDistrictsStack );
-local m_kTradingPostsIM		:table = InstanceManager:new( "TradingPostInstance",		"Top", Controls.TradingPostsStack );
-local m_kWondersIM			:table = InstanceManager:new( "WonderInstance",				"Top", Controls.WondersStack );
+for _, sVersion in ipairs(tCityBannerVersions) do
+    include(sVersion)
+    if Initialize then
+        print("CityPanelOverview_CityPolicies loading " .. sVersion .. " as base file")
+        break
+    end
+end
+
+Base_LateInitialize = LateInitialize;
+Base_ViewPanelBreakdown = ViewPanelBreakdown;
 
 -- ===========================================================================
 function ViewPanelBreakdown( data:table )
-	Controls.DistrictsNum:SetText( data.DistrictsNum );
-	Controls.DistrictsConstructed:SetText( Locale.Lookup("LOC_HUD_CITY_DISTRICTS_CONSTRUCTED", data.DistrictsNum) );	
-	Controls.DistrictsPossibleNum:SetText( data.DistrictsPossibleNum );
+    -- Duplicate current data for modification.
+    local modifiedData = deepcopy(data, {});
 
-	m_kBuildingsIM:ResetInstances();
-	m_kDistrictsIM:ResetInstances();	
-	m_kTradingPostsIM:ResetInstances();
-	m_kWondersIM:ResetInstances();
-	local playerID = Game.GetLocalPlayer();
+    -- Change modifiedData's all CITY_POLICY buildings to be "not built". Can also remove them but harder to do when iterating the table.
+    for _, district in ipairs(modifiedData.BuildingsAndDistricts) do
+        if district.isBuilt and district.Type == "DISTRICT_CITY_CENTER" then
+            for _,building in ipairs(district.Buildings) do
+                if building.isBuilt and IsCityPolicy(building) then
+                    building.isBuilt = false;
+                end
+            end
+        end
+    end
 
-	-- Add districts (and their buildings)
-	for _, district in ipairs(data.BuildingsAndDistricts) do
-		if district.isBuilt then
-			local kInstanceDistrict:table = m_kDistrictsIM:GetInstance();
-			local districtName = district.Name;
-			if district.isPillaged then
-				districtName = districtName .. "[ICON_Pillaged]";
-			end
-			kInstanceDistrict.DistrictName:SetText( districtName );
-			kInstanceDistrict.DistrictYield:SetText( district.YieldBonus );
-			kInstanceDistrict.Icon:SetIcon( district.Icon );
-			local sToolTip = ToolTipHelper.GetToolTip(district.Type, playerID)
-			kInstanceDistrict.Top:SetToolTipString( sToolTip);
-			for _,building in ipairs(district.Buildings) do
-				if building.isBuilt and not IsCityPolicy(building) then -- City policy changes
-					local kInstanceBuild:table = m_kBuildingsIM:GetInstance(kInstanceDistrict.BuildingStack);
-					local buildingName = building.Name;
-					if building.isPillaged then
-						buildingName = buildingName .. "[ICON_Pillaged]";
-					end
-					kInstanceBuild.BuildingName:SetText( buildingName );
-					kInstanceBuild.Icon:SetIcon( building.Icon );
-					local pRow = GameInfo.Buildings[building.Type];
-					local sToolTip = ToolTipHelper.GetBuildingToolTip( pRow.Hash, playerID, m_pCity );
-					kInstanceBuild.Top:SetToolTipString( sToolTip);
-					local yieldString:string = "";
-					for _,kYield in ipairs(building.Yields) do
-						yieldString = yieldString .. GetYieldString(kYield.YieldType,kYield.YieldChange);
-					end
-					kInstanceBuild.BuildingYield:SetText( yieldString );
-					kInstanceBuild.BuildingYield:SetTruncateWidth( kInstanceBuild.Top:GetSizeX() - kInstanceBuild.BuildingName:GetSizeX() - 10 );
-				end
-			end
-			kInstanceDistrict.BuildingStack:CalculateSize();
-		end
-	end
+    -- Construct the panel using default ViewPanelBreakdown with modified data, which won't render city policy data.
+    Base_ViewPanelBreakdown(modifiedData);
 
-	-- Add wonders
-	local hideWondersInfo :boolean = not GameCapabilities.HasCapability("CAPABILITY_CITY_HUD_WONDERS");
-	local isHasWonders :boolean = (table.count(data.Wonders) > 0)
-	Controls.NoWondersArea:SetHide(hideWondersInfo or isHasWonders);
-	Controls.WondersArea:SetHide(hideWondersInfo or not isHasWonders);
-	Controls.WondersHeader:SetHide(hideWondersInfo);
-
-	for _, wonder in ipairs(data.Wonders) do
-		local kInstanceWonder:table = m_kWondersIM:GetInstance();
-		kInstanceWonder.WonderName:SetText( wonder.Name );
-		local pRow = GameInfo.Buildings[wonder.Type];
-		local sToolTip = ToolTipHelper.GetBuildingToolTip( pRow.Hash, playerID, m_pCity );
-		kInstanceWonder.Top:SetToolTipString( sToolTip );
-		local yieldString:string = "";
-		for _,kYield in ipairs(wonder.Yields) do
-			yieldString = yieldString .. GetYieldString(kYield.YieldType,kYield.YieldChange);
-		end
-		kInstanceWonder.WonderYield:SetText( yieldString );
-		kInstanceWonder.Icon:SetIcon( wonder.Icon );
-	end
-
-	-- Add trading posts
-	local hideTradingPostsInfo :boolean = not GameCapabilities.HasCapability("CAPABILITY_CITY_HUD_TRADING_POSTS");
-	local isHasTradingPosts :boolean = (table.count(data.TradingPosts) > 0);
-	Controls.NoTradingPostsArea:SetHide(hideTradingPostsInfo or isHasTradingPosts);
-	Controls.TradingPostsArea:SetHide(hideTradingPostsInfo or not isHasTradingPosts);
-	Controls.TradingPostsHeader:SetHide(hideTradingPostsInfo);
-
-	if isHasTradingPosts then
-		for _, tradePostPlayerId in ipairs(data.TradingPosts) do
-			local pTradePostPlayer:table = Players[tradePostPlayerId]
-			local pTradePostPlayerConfig:table = PlayerConfigurations[tradePostPlayerId];
-			local kInstanceTradingPost	:table = m_kTradingPostsIM:GetInstance();		
-			local playerName			:string = Locale.Lookup( pTradePostPlayerConfig:GetPlayerName() );
-
-			local iconName:string = "";
-			local iconSize:number = SIZE_LEADER_ICON;
-			local iconColor = UI.GetColorValue("COLOR_WHITE");
-			if pTradePostPlayer:IsMinor() then
-				-- If we're a city-state display our city-state icon instead of leader since we don't have one
-				local civType:string = pTradePostPlayerConfig:GetCivilizationTypeName();
-				local primaryColor, secondaryColor = UI.GetPlayerColors(tradePostPlayerId);	
-				iconName = "ICON_"..civType;
-				iconColor = secondaryColor;
-				iconSize = SIZE_CITYSTATE_ICON;
-			else
-				iconName = "ICON_"..pTradePostPlayerConfig:GetLeaderTypeName();
-			end
-			
-			local textureOffsetX :number, textureOffsetY:number, textureSheet:string = IconManager:FindIconAtlas(iconName, iconSize);
-			kInstanceTradingPost.LeaderPortrait:SetTexture(textureOffsetX, textureOffsetY, textureSheet);
-			kInstanceTradingPost.LeaderPortrait:SetColor(iconColor);
-			kInstanceTradingPost.LeaderPortrait:SetHide(false);
-							
-			if tradePostPlayerId == m_pPlayer:GetID() then
-				playerName = playerName .. " (" .. Locale.Lookup("LOC_HUD_CITY_YOU") .. ")";
-			end
-			kInstanceTradingPost.TradingPostName:SetText( playerName );
-		end
-	end
-
-	-- Add city policy
-	LuaEvents.CityPanelOverview_CityPolicies(playerID, data)
+    -- Add city policy (process with original data)
+    LuaEvents.CityPanelOverview_CityPolicies(data);
 end
 
--- ===========================================================================
--- City Policies Mod addition
--- ===========================================================================
 function IsCityPolicy( building:table )
-	local buildingType:string = building.Type;
-	return buildingType:match "^BUILDING_CITY_POLICY_";	
+    local buildingType:string = building.Type;
+    return buildingType:match "^BUILDING_CITY_POLICY_";    
+end
+
+-- Deepcopying table. Taken from http://lua-users.org/wiki/CopyTable.
+function deepcopy(orig, copies)
+    copies = copies or {}
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        if copies[orig] then
+            copy = copies[orig]
+        else
+            copy = {}
+            copies[orig] = copy
+            for orig_key, orig_value in next, orig, nil do
+                copy[deepcopy(orig_key, copies)] = deepcopy(orig_value, copies)
+            end
+            setmetatable(copy, deepcopy(getmetatable(orig), copies))
+        end
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
+
+function LateInitialize()
+    Base_LateInitialize();
+
+    -- Fix TradingPostsHeader
+    for _, child in ipairs(Controls.TradingPostsHeader:GetChildren()) do
+        -- Set Trading Post Label's offsetX from 10 to 0. The other child has offsetX of 0 by default.
+        child:SetOffsetX(0);
+    end
 end
--- CityPolicies/UI/CityPanelOverview_CityPolicies_Instances.lua
+++ CityPolicies/UI/CityPanelOverview_CityPolicies_Instances.lua
@@ -3,81 +3,82 @@ include( "InstanceManager" );
 include( "ToolTipHelper" );
 
 -- ===========================================================================
---	MEMBERS
+-- MEMBERS
 -- ===========================================================================
-pBreakdownStack	= nil
+pBreakdownStack    = nil
 
-local m_kCityPolicyIM:table = InstanceManager:new("CityPolicyInstance",	"Top", Controls.CityPolicyStack);
+local m_kCityPolicyIM:table = InstanceManager:new("CityPolicyInstance",    "Top", Controls.CityPolicyStack);
 
 -- ===========================================================================
---	CityPanelOverview_CityPolicies
+-- CityPanelOverview_CityPolicies
 -- ===========================================================================
-function On_CityPanelOverview_CityPolicies(player, data)
+function On_CityPanelOverview_CityPolicies(data)
+    local playerID = Game.GetLocalPlayer();
 
-	local hasCityPolicies = false;
-	m_kCityPolicyIM:ResetInstances();
-	
-	for _, district in ipairs(data.BuildingsAndDistricts) do
-		if district.isBuilt and district.Type == "DISTRICT_CITY_CENTER" then
-			for _,building in ipairs(district.Buildings) do
-				if building.isBuilt then
-					local buildingType = building.Type;
-					if buildingType == "BUILDING_CITY_POLICY_EMPTY" then
-						-- No city policies have been established.
-						hasCityPolicies = false;
-						break;
-					elseif buildingType:match "^BUILDING_CITY_POLICY_" then
-						-- Add active city policy
-						hasCityPolicies = true;
-						local cityPolicyInstance:table = m_kCityPolicyIM:GetInstance();
-						cityPolicyInstance.CityPolicyName:SetText(building.Name);
-						cityPolicyInstance.Icon:SetIcon(building.Icon);
-						-- TODO: Add proper tooltip for city policy
-						local pRow = GameInfo.Buildings[buildingType];
-						local sToolTip = ToolTipHelper.GetBuildingToolTip(pRow.Hash, player, m_pCity);
-						cityPolicyInstance.Top:SetToolTipString(sToolTip);
-					end
-				end
-			end
-			Controls.CityPolicyStack:CalculateSize();
-		end
-	end
+    local hasCityPolicies = false;
+    m_kCityPolicyIM:ResetInstances();
 
-	Controls.NoCityPolicyArea:SetHide(hasCityPolicies);
-	Controls.CityPolicyArea:SetHide(not hasCityPolicies);
+    for _, district in ipairs(data.BuildingsAndDistricts) do
+        if district.isBuilt and district.Type == "DISTRICT_CITY_CENTER" then
+            for _,building in ipairs(district.Buildings) do
+                if building.isBuilt then
+                    local buildingType = building.Type;
+                    if buildingType == "BUILDING_CITY_POLICY_EMPTY" then
+                        -- No city policies have been established.
+                        hasCityPolicies = false;
+                        break;
+                    elseif buildingType:match "^BUILDING_CITY_POLICY_" then
+                        -- Add active city policy
+                        hasCityPolicies = true;
+                        local cityPolicyInstance:table = m_kCityPolicyIM:GetInstance();
+                        cityPolicyInstance.CityPolicyName:SetText(building.Name);
+                        cityPolicyInstance.Icon:SetIcon(building.Icon);
+                        -- TODO: Add proper tooltip for city policy
+                        local pRow = GameInfo.Buildings[buildingType];
+                        local sToolTip = ToolTipHelper.GetBuildingToolTip(pRow.Hash, playerID, m_pCity);
+                        cityPolicyInstance.Top:SetToolTipString(sToolTip);
+                    end
+                end
+            end
+            Controls.CityPolicyStack:CalculateSize();
+        end
+    end
+
+    Controls.NoCityPolicyArea:SetHide(hasCityPolicies);
+    Controls.CityPolicyArea:SetHide(not hasCityPolicies);
 end
 
 -- ===========================================================================
---	Init and Shutdown
+--    Init and Shutdown
 -- ===========================================================================
 local bLoadScreenHookup = false
 
 function OnInit(isReload)
-	if not ContextPtr:LookUpControl("/InGame/CityPanelOverview/BreakdownStack") then
-		Events.LoadScreenClose.Add(OnInit)
-		bLoadScreenHookup = true
-		return
-	end
+    if not ContextPtr:LookUpControl("/InGame/CityPanelOverview/BreakdownStack") then
+        Events.LoadScreenClose.Add(OnInit)
+        bLoadScreenHookup = true
+        return
+    end
 
-	-- Attach CityPolicyContainer to BreakdownStack in CityPanelOverview
-	pBreakdownStack	= ContextPtr:LookUpControl("/InGame/CityPanelOverview/BreakdownStack")
-	Controls.CityPolicyContainer:ChangeParent(pBreakdownStack)
+    -- Attach CityPolicyContainer to BreakdownStack in CityPanelOverview
+    pBreakdownStack    = ContextPtr:LookUpControl("/InGame/CityPanelOverview/BreakdownStack")
+    Controls.CityPolicyContainer:ChangeParent(pBreakdownStack)
 
-	LuaEvents.CityPanelOverview_CityPolicies.Add(On_CityPanelOverview_CityPolicies)
+    LuaEvents.CityPanelOverview_CityPolicies.Add(On_CityPanelOverview_CityPolicies)
 end
 
 function OnShutdown()
-	if bLoadScreenHookup then Events.LoadScreenClose.Remove(OnInit) end
-	LuaEvents.CityPanelOverview_CityPolicies.Remove(On_CityPanelOverview_CityPolicies)
+    if bLoadScreenHookup then Events.LoadScreenClose.Remove(OnInit) end
+    LuaEvents.CityPanelOverview_CityPolicies.Remove(On_CityPanelOverview_CityPolicies)
 
-	m_kCityPolicyIM:DestroyInstances()
-	-- Detach CityPolicyContainer
-	pBreakdownStack:DestroyChild(Controls.CityPolicyContainer)
+    m_kCityPolicyIM:DestroyInstances()
+    -- Detach CityPolicyContainer
+    pBreakdownStack:DestroyChild(Controls.CityPolicyContainer)
 end
 -- ===========================================================================
 function Initialize()
-	ContextPtr:SetInitHandler(OnInit)
-	ContextPtr:SetHide(false)
-	ContextPtr:SetShutdown(OnShutdown)
+    ContextPtr:SetInitHandler(OnInit)
+    ContextPtr:SetHide(false)
+    ContextPtr:SetShutdown(OnShutdown)
 end
 Initialize()
