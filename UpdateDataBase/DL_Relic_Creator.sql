3a2aea997741d2e125e9faf539089c2a41b93362
wltk <>
2021-02-20 23:08:04 -0800

    Grant relic for 'To the glory of god' by 33%
    1. Registered listener for UnitGreatPersonActivated event. Check if
       great person is fully used and player has 'to the glory of god'
       belief before rolling 33% to grant relic.
    2. Created dummy building RELIC_CREATOR to help with relic granting.
    3. Cancel notification for the granted relic to prevent showing Relic
       screen twice.

---
+++ UpdateDataBase/DL_Relic_Creator.sql
@@ -0,0 +1,16 @@
+-- Adding Relic Creator building as a dummy building. i.e. InternalOnly = true.
+INSERT INTO Types (Type, Kind) VALUES
+('BUILDING_RELIC_CREATOR', 'KIND_BUILDING');
+
+INSERT INTO Buildings (BuildingType, Name, Cost, PrereqDistrict, Description, InternalOnly) VALUES
+('BUILDING_RELIC_CREATOR', 'LOC_BUILDING_RELIC_CREATOR_NAME', 1, 'DISTRICT_CITY_CENTER', 'LOC_BUILDING_RELIC_CREATOR_DESCRIPTION', 1);
+
+-- Attach grant relic modifier to Relic Creator building
+INSERT INTO BuildingModifiers (BuildingType, ModifierId) VALUES
+('BUILDING_RELIC_CREATOR', 'MODIFIER_RELIC_CREATOR');
+
+INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent, NewOnly, OwnerRequirementSetId, SubjectRequirementSetId) VALUES
+('MODIFIER_RELIC_CREATOR', 'MODIFIER_PLAYER_GRANT_RELIC', 1, 1, 0, NULL, NULL);
+
+INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES
+('MODIFIER_RELIC_CREATOR', 'Amount', '1');
