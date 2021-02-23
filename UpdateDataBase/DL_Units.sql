a32aa5d91d07de8b4e4cbaef54a9cb3f8eefde70
xiaoyu <3334559688@qq.com>
2021-02-23 16:44:41 +0800

    amani promotion update
    1. fix bug that Liang's promotion does not provide GEP
    2. Amani promotion and text update

--- UpdateDataBase/DL_Units.sql
+++ UpdateDataBase/DL_Units.sql
@@ -60,5 +60,5 @@ update Units set ObsoleteCivic = null where UnitType = 'UNIT_BATTERING_RAM';
 
 --worrior monk and nihang can have siege_tower and Battering_ram bonus
 --to do
-update Units set PrereqCivic = 'CIVIC_THEOLOGY' where UnitType = 'UNIT_WARRIOR_MONK';
+--update Units set PrereqCivic = 'CIVIC_THEOLOGY' where UnitType = 'UNIT_WARRIOR_MONK';
 	
