481a212996da30d89f2dee66598b0488f8c27aeb
wltk <>
2021-02-22 21:41:31 -0800

    Apply yield to city directly instead of through dummy buildings
    1. Apply yield modifier directly to city instead of through dummy
       buildings and workers.
    2. Change AddCityYield to ChangeCityYield, change city's yield to the
       given amount. Cache all cities' yields to assist this method.
    3. Add modifiers for other resource types instead of just gold.

--- Gameplay/Common.lua
+++ Gameplay/Common.lua
@@ -5,57 +5,47 @@
 -- Available numbers in descending order.
 local m_YieldAvailableNums = { 10, 5, 2, 1 };
 
-function ClearCityYield(playerID, cityID)
+-- Cache city yield in this format:
+-- {
+--      "123" = {               -- CityID
+--          "YIELD_GOLD" = 10,
+--          "YIELD_FOOD" = 5,
+--          ...
+--      }
+--      ...
+-- }
+--
+local m_CachedCityYield = {};
+
+-- Change city yield to the given amount.
+function ChangeCityYield(playerID, cityID, yieldAmount, yieldType)
     local city:table = CityManager.GetCity(playerID, cityID);
-    if city ~= nil then
-        local buildings = city:GetBuildings();
-        local yieldCreatorIndex = GameInfo.Buildings["BUILDING_YIELD_CREATOR"].Index;
-        buildings:RemoveBuilding(yieldCreatorIndex);
-    end
-end
-
-function AddCityYield(playerID, cityID, yieldAmount, yieldType)
-    local city:table = CityManager.GetCity(playerID, cityID);
 
     if city ~= nil then
-        local buildings = city:GetBuildings();
-        local buildingQueue = city:GetBuildQueue();
+        local deltaYield = yieldAmount;
 
-        -- Create Yield Creator if it doesn't exist.
-        local yieldCreatorIndex = GameInfo.Buildings["BUILDING_YIELD_CREATOR"].Index;
-        -- Remove Relic Creator from the city just in case.
-        if not buildings:HasBuilding(yieldCreatorIndex) then
-            buildingQueue:CreateBuilding(yieldCreatorIndex);
+        local cityYield = m_CachedCityYield[cityID];
+        if cityYield ~= nil then
+            local currentYield = cityYield[yieldType];
+            if currentYield ~= nil then
+                deltaYield = yieldAmount - currentYield;
+            end
         end
+        local modifierList = GetModifierList(deltaYield, yieldType);
 
-        -- Get yield creator workers for the given yield amount and type.
-        local yieldName = nil;
-        if yieldType == YieldTypes.CULTURE then 
-            yieldName = "CULTURE";
-        elseif yieldType == YieldTypes.FAITH then 
-            yieldName = "FAITH";
-        elseif yieldType == YieldTypes.FOOD then 
-            yieldName = "FOOD";
-        elseif yieldType == YieldTypes.GOLD then 
-            yieldName = "GOLD";
-        elseif yieldType == YieldTypes.PRODUCTION then 
-            yieldName = "PRODUCTION";
-        elseif yieldType == YieldTypes.SCIENCE then 
-            yieldName = "SCIENCE";
-        end
-        local modifierList = GetModifierList(yieldAmount, yieldName);
-
-        -- Grant the yields to the Yield Creator building.
+        -- Grant the yields to the city.
         for _, modifier in ipairs(modifierList) do
-            local index = GameInfo.Buildings[modifier].Index;
-            buildingQueue:CreateBuilding(index);
-            buildings:RemoveBuilding(index);
+            city:AttachModifierByID(modifier);
         end
+
+        -- Update local cache.
+        m_CachedCityYield[cityID] = m_CachedCityYield[cityID] or {};
+        m_CachedCityYield[cityID][yieldType] = yieldAmount;
     end
 end
 
 -- Helper function to get list of modifiers to be used to get the yield amount using recursion.
-function GetModifierList(yieldAmount, yieldName)
+function GetModifierList(yieldAmount, yieldType)
     if yieldAmount == 0 then
         return {};
     end
@@ -69,8 +59,9 @@ function GetModifierList(yieldAmount, yieldName)
     -- Start trying with available numbers.
     for _, number in ipairs(m_YieldAvailableNums) do
         if number <= absAmount then
+            local yieldName = string.gsub(yieldType, "YIELD_", "");
             -- Use the modifier of this number
-            local modifierBuilding = "BUILDING_YIELD_CREATOR_WORKER_" .. yieldName .. "_" .. number;
+            local modifierBuilding = "YIELD_CREATOR_" .. yieldName .. "_" .. number;
             local newNum = absAmount - number;
             if isNegative then
                 modifierBuilding = modifierBuilding .. "_N";
