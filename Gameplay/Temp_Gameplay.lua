c78f3ac5b10198d06ce4a480adb1d639e0f85c6d
xiaoyu <3334559688@qq.com>
2021-02-25 22:13:08 +0800

    small update ( with other update)
    1. changelog没写 用这个代替
    2. 埃及能力移除泛滥区域上的粮食
    3. 强化信条虔诚商队新增：所有商路+2信仰
    4. 新增所有文明下城时获得金币：数值等于被占领城市人口的20倍。如果该城市为首都则翻倍。若玩家有军阀宝座则在此基础上再+50%金币收益
    （4需要debug目前实现了第一部分内容）

--- Gameplay/Temp_Gameplay.lua
+++ Gameplay/Temp_Gameplay.lua
@@ -13,29 +13,29 @@ end
 -----------------------------------------------------------------------
 
 -- Start debug
-function EarnMoneyOnConquerCity( capturerID, ownerID, cityID, cityX, cityY )
-	print( 'capture city' )
-	local cPlayer = Players[ownerID]
-	local pPlayer = Players[capturerID]
-	local ConquerCity = CityManager.GetCityAt(cityX, cityY)
-	local citizen = ConquerCity:GetPopulation()
-	if cPlayer ~= nil and pPlayer ~= nil and ConquerCity ~= nil then	
-	 	local amount = citizen * 20 
-		Utils.ChangeGoldBalance(capturerID, amount)
-		if (ConquerCity:IsCapital() and cPlayer:IsMajor()) then
-			local amount_capital = citizen * 20
-			Utils.ChangeGoldBalance(capturerID, amount_capital) 
-			if (Utils.HasBuildingWithinCountry(playerID, cityID )) then
-				local amount_capitalandgov = citizen * 20
-				Utils.ChangeGoldBalance(capturerID, amount_capitalandgov) 
-				return
-			end
-		end
-		if (Utils.HasBuildingWithinCountry(playerID, cityID )) then
-			local amount_gov = citizen * 10
-			Utils.ChangeGoldBalance(capturerID, amount_gov)
-		end
-	end
-end
+-- function EarnMoneyOnConquerCity( capturerID, ownerID, cityID, cityX, cityY )
+-- 	print( 'capture city' )
+-- 	local cPlayer = Players[ownerID]
+-- 	local pPlayer = Players[capturerID]
+-- 	local ConquerCity = CityManager.GetCityAt(cityX, cityY)
+-- 	local citizen = ConquerCity:GetPopulation()
+-- 	if cPlayer ~= nil and pPlayer ~= nil and ConquerCity ~= nil then	
+-- 	 	local amount = citizen * 20 
+-- 		Utils.ChangeGoldBalance(capturerID, amount)
+-- 		if (ConquerCity:IsCapital() and cPlayer:IsMajor()) then
+-- 			local amount_capital = citizen * 20
+-- 			Utils.ChangeGoldBalance(capturerID, amount_capital) 
+-- 			if (Utils.HasBuildingWithinCountry(playerID, cityID )) then
+-- 				local amount_capitalandgov = citizen * 20
+-- 				Utils.ChangeGoldBalance(capturerID, amount_capitalandgov) 
+-- 				return
+-- 			end
+-- 		end
+-- 		if (Utils.HasBuildingWithinCountry(playerID, cityID )) then
+-- 			local amount_gov = citizen * 10
+-- 			Utils.ChangeGoldBalance(capturerID, amount_gov)
+-- 		end
+-- 	end
+-- end
 
-GameEvents.CityConquered.Add(EarnMoneyOnConquerCity)
+-- GameEvents.CityConquered.Add(EarnMoneyOnConquerCity)
