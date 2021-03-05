2590d4bfaee29cfefa7e35da02390758f4db2f1f
wltk <>
2021-03-04 19:13:30 -0800

    Add existing modifiers parsing during loading for backwards compatibility. Add method for GetYield.

--- Gameplay/CityYield.lua
+++ Gameplay/CityYield.lua
@@ -13,7 +13,7 @@ CityYield = {};
 -- }
 -- ==================================================================
 
-local m_PropKey_CityYield = "CityYield";
+local PROP_KEY_CITY_YIELD = "CityYield";
 local MAX_YIELD_MODIFIER_VALUE = 50;
 
 -- Source type constants.
@@ -34,12 +34,37 @@ CityYield.ClearYield = function(playerID, cityID, sourceType)
     end
 end
 
+-- Get the given city's yield for the given yield type and source type.
+CityYield.GetYield = function(playerID, cityID, yieldType, sourceType)
+    local city:table = CityManager.GetCity(playerID, cityID);
+    
+    if city ~= nil then
+        local cityProps:table = city:GetProperty(PROP_KEY_CITY_YIELD);
+        if cityProps == nil then
+            cityProps = {};
+        end
+
+        sourceType = sourceType or CityYield.TYPE_DEFAULT;
+
+        local yieldName = string.gsub(yieldType, "YIELD_", "");
+        local cityYield = cityProps[sourceType];
+        if cityYield ~= nil then
+            local currentYield = cityYield[yieldName];
+            if currentYield ~= nil then
+                return currentYield;
+            end
+        end
+    end
+
+    return 0;
+end
+
 -- Change the given city's yield to the given amount for the specific source type.
 CityYield.ChangeYield = function(playerID, cityID, yieldAmount, yieldType, sourceType)
     local city:table = CityManager.GetCity(playerID, cityID);
 
     if city ~= nil then
-        local cityProps:table = city:GetProperty(m_PropKey_CityYield);
+        local cityProps:table = city:GetProperty(PROP_KEY_CITY_YIELD);
         if cityProps == nil then
             cityProps = {};
         end
@@ -70,7 +95,7 @@ CityYield.ChangeYield = function(playerID, cityID, yieldAmount, yieldType, sourc
         cityYield = cityYield or {};
         cityYield[yieldName] = yieldAmount;
         cityProps[sourceType] = cityYield;
-        city:SetProperty(m_PropKey_CityYield, cityProps);
+        city:SetProperty(PROP_KEY_CITY_YIELD, cityProps);
     end
 end
 
@@ -102,6 +127,50 @@ function GetModifierList(yieldAmount, yieldName)
 
     return result;
 end
+
+-- Temporary code for backward compatibility.
+function OnGameLoadPopulateCityYieldCache()
+    local isFirstCheck = true;
+    -- Get available modifiers.
+    local modifiers = GameEffects.GetModifiers();
+    for i, modifier in ipairs(modifiers) do
+        local modifierID = GameEffects.GetModifierDefinition(modifier).Id;
+
+        if modifierID:match "^YIELD_CREATOR_" then
+            -- Yield creator modifiers
+            local yieldInfo = string.gsub(modifierID, "YIELD_CREATOR_", "");
+            local type, amount, neg = yieldInfo:match("([^_]+)_(%d+)_?([^%d]?)");
+            amount = tonumber(amount);
+            if neg == "N" then
+                amount = -amount;
+            end
+
+            -- Get the owner and city id.
+            local owner = GameEffects.GetModifierOwner(modifier);
+            local ownerString = GameEffects.GetObjectString(owner);
+            -- The string is like: City (65536), Owner: 0, Name: LOC_CITY_NAME_AMSTERDAM
+            local cityID, ownerID = ownerString:match("[^%d]*(%d+)[^%d]*(%d+).*");
+
+            local city:table = CityManager.GetCity(ownerID, cityID);
+
+            if city ~= nil then
+                local cityProps:table = city:GetProperty(PROP_KEY_CITY_YIELD);
+                if isFirstCheck and cityProps ~= nil then return end
+                isFirstCheck = false;
+
+                -- Update cache
+                cityProps = cityProps or {};
+                cityProps[CityYield.TYPE_MULTINATIONAL_CORP] = cityProps[CityYield.TYPE_MULTINATIONAL_CORP] or {};
+                cityProps[CityYield.TYPE_MULTINATIONAL_CORP][type] = cityProps[CityYield.TYPE_MULTINATIONAL_CORP][type] or 0;
+                -- Add all modifiers' amount together.
+                cityProps[CityYield.TYPE_MULTINATIONAL_CORP][type] = cityProps[CityYield.TYPE_MULTINATIONAL_CORP][type] + amount;
+                city:SetProperty(PROP_KEY_CITY_YIELD, cityProps);
+            end
+        end
+    end
+end
+
+Events.LoadScreenClose.Add(OnGameLoadPopulateCityYieldCache);
 
 ExposedMembers.DLHD = ExposedMembers.DLHD or {};
 ExposedMembers.DLHD.CityYield = CityYield;
