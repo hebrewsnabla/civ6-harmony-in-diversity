38ed590ab5d9679f2269f6bc3a2863147d97e423
DeepLogic <>
2021-03-11 12:07:10 +0800

    move unitpanel out of FOFH

--- UI/Replacement/DL_UnitPanel.lua
+++ UI/Replacement/DL_UnitPanel.lua
@@ -10,7 +10,7 @@ local files = {
 for _, file in ipairs(files) do
     include(file)
     if Initialize then
-        print("FOFH Loading " .. file .. " as base file");
+        print("DL Loading " .. file .. " as base file");
         break
     end
 end
@@ -60,3 +60,36 @@ function BuildActionModHook(instance:table, action:table)
 
     BASE_BuildActionModHook(instance, action);
 end
+
+-- -- Temp for great person
+-- local BASE_AddActionButton = AddActionButton
+-- local BASE_OnUnitActionClicked = OnUnitActionClicked
+-- local mGreatPersonActivateHash = GameInfo.Types['UNITCOMMAND_ACTIVATE_GREAT_PERSON'].Hash;
+-- -- =================================================================================
+-- -- Overrides
+-- -- =================================================================================
+-- function OnUnitActionClicked( actionType:number, actionHash:number, currentMode:number )
+--     -- print('OnUnitActionClicked', actionType, actionHash) -- OnUnitActionClicked -1572680103 374670040
+--     if g_isOkayToProcess then
+--         local pSelectedUnit :table = UI.GetHeadSelectedUnit();
+--         if (pSelectedUnit ~= nil) then
+--             if (actionType == UnitCommandTypes.TYPE) then
+--                 if actionHash == mGreatPersonActivateHash then
+--                     -- pSelectedUnit
+--                     UnitManager.RequestCommand( pSelectedUnit, actionHash );
+--                 end
+--             end
+--         end
+--     end
+--     BASE_OnUnitActionClicked(actionType, actionHash, currentMode)
+-- end
+
+-- function AddActionButton( instance:table, action:table )
+--     -- print('AddActionButton', action.IconId)
+--     if action.IconId == 'ICON_UNITOPERATION_MOVE_TO' then
+--         action.Disabled = true
+--         local pSelectedUnit :table= UI.GetHeadSelectedUnit();
+--         print(pSelectedUnit)
+--     end
+--     BASE_AddActionButton(instance, action)
+-- end
