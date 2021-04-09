4adba22113c45be68b711c730855d027a21f7c1b
DeepLogic <>
2021-04-09 20:22:17 +0800

    [v0.7.1.3] add concise ui citystates panel as third party

---
+++ ThirdParty/ConciseUICityStates/localization/cui_config.sql
@@ -0,0 +1,164 @@
+-- ===========================================================================
+-- CUI Config Text File
+-- ===========================================================================
+
+INSERT OR REPLACE INTO LocalizedText (Tag, Language, Text) VALUES
+
+-- ===========================================================================
+-- [Deutsch] by [Titule]
+-- ===========================================================================
+("LOC_CUI_CONFIG_CATEGORY",            "de_DE", "Concise UI"),
+("LOC_CUI_CONFIG_PLACE_MAP_PIN",       "de_DE", "Kartennadel hinzufügen"),
+("LOC_CUI_CONFIG_TOGGLE_IMPROVED",     "de_DE", "Strategische Resourcen ein/aus"),
+("LOC_CUI_CONFIG_TOGGLE_UNIT_FLAGS",   "de_DE", "Einheitenflaggen ein/aus"),
+("LOC_CUI_CONFIG_TOGGLE_TRADERS",      "de_DE", "Händler ein/aus"),
+("LOC_CUI_CONFIG_TOGGLE_RELIGIONS",    "de_DE", "Religionseinheiten ein/aus"),
+("LOC_CUI_CONFIG_TOGGLE_CITY_BANNERS", "de_DE", "Städtebanner ein/aus"),
+("LOC_CUI_CONFIG_OPEN_UNIT_LIST",      "de_DE", "Einheitenliste öffnen"),
+("LOC_CUI_CONFIG_OPEN_NOTES",          "de_DE", "Notizen öffnen"),
+
+-- ===========================================================================
+-- [English] by [eudaimonia]
+-- ===========================================================================
+("LOC_CUI_CONFIG_CATEGORY",            "en_US", "Concise UI"),
+("LOC_CUI_CONFIG_PLACE_MAP_PIN",       "en_US", "Add Map Tack"),
+("LOC_CUI_CONFIG_TOGGLE_IMPROVED",     "en_US", "Toggle Improved Resource"),
+("LOC_CUI_CONFIG_TOGGLE_UNIT_FLAGS",   "en_US", "Toggle Unit Flags"),
+("LOC_CUI_CONFIG_TOGGLE_TRADERS",      "en_US", "Toggle Trader Icons"),
+("LOC_CUI_CONFIG_TOGGLE_RELIGIONS",    "en_US", "Toggle Religion Icons"),
+("LOC_CUI_CONFIG_TOGGLE_CITY_BANNERS", "en_US", "Toggle City Banners"),
+("LOC_CUI_CONFIG_OPEN_UNIT_LIST",      "en_US", "Open Unit List"),
+("LOC_CUI_CONFIG_OPEN_NOTES",          "en_US", "Open Notes"),
+
+-- ===========================================================================
+-- [Spanish] by [MinuZzzZz] 4/26/2019
+-- ===========================================================================
+("LOC_CUI_CONFIG_CATEGORY",            "es_ES", "Concise UI"),
+("LOC_CUI_CONFIG_PLACE_MAP_PIN",       "es_ES", "Añadir Marcador de mapa"),
+("LOC_CUI_CONFIG_TOGGLE_IMPROVED",     "es_ES", "Alternar Recursos Mejorados"),
+("LOC_CUI_CONFIG_TOGGLE_UNIT_FLAGS",   "es_ES", "Alternar Banderas de unidad"),
+("LOC_CUI_CONFIG_TOGGLE_TRADERS",      "es_ES", "Alternar Iconos de comerciante"),
+("LOC_CUI_CONFIG_TOGGLE_RELIGIONS",    "es_ES", "Toggle Religion Icons"),
+("LOC_CUI_CONFIG_TOGGLE_CITY_BANNERS", "es_ES", "Alternar Estandartes de ciudad"),
+("LOC_CUI_CONFIG_OPEN_UNIT_LIST",      "es_ES", "Abrir lista de unidades"),
+("LOC_CUI_CONFIG_OPEN_NOTES",          "es_ES", "Open Notes"),
+
+-- ===========================================================================
+-- [French] by [G.] 3/1/2019
+-- ===========================================================================
+("LOC_CUI_CONFIG_CATEGORY",            "fr_FR", "Concise UI"),
+("LOC_CUI_CONFIG_PLACE_MAP_PIN",       "fr_FR", "Ajouter un marqueur"),
+("LOC_CUI_CONFIG_TOGGLE_IMPROVED",     "fr_FR", "Afficher les ressources aménagées"),
+("LOC_CUI_CONFIG_TOGGLE_UNIT_FLAGS",   "fr_FR", "Afficher les icônes des unités"),
+("LOC_CUI_CONFIG_TOGGLE_TRADERS",      "fr_FR", "Afficher les icônes des Négociants"),
+("LOC_CUI_CONFIG_TOGGLE_RELIGIONS",    "fr_FR", "Toggle Religion Icons"),
+("LOC_CUI_CONFIG_TOGGLE_CITY_BANNERS", "fr_FR", "Afficher les bannières (noms) des villes"),
+("LOC_CUI_CONFIG_OPEN_UNIT_LIST",      "fr_FR", "Ouvrir la liste des unités"),
+("LOC_CUI_CONFIG_OPEN_NOTES",          "fr_FR", "Open Notes"),
+
+-- ===========================================================================
+-- [Italiano] by [Diaz Ex Machina] 2/18/2019
+-- ===========================================================================
+("LOC_CUI_CONFIG_CATEGORY",            "it_IT", "Concise UI"),
+("LOC_CUI_CONFIG_PLACE_MAP_PIN",       "it_IT", "Aggiungi spillo"),
+("LOC_CUI_CONFIG_TOGGLE_IMPROVED",     "it_IT", "Mostra/nascondi risorse migliorate"),
+("LOC_CUI_CONFIG_TOGGLE_UNIT_FLAGS",   "it_IT", "Mostra/nascondi segnaposto unità"),
+("LOC_CUI_CONFIG_TOGGLE_TRADERS",      "it_IT", "Mostra/nascondi icone commerciante"),
+("LOC_CUI_CONFIG_TOGGLE_RELIGIONS",    "it_IT", "Toggle Religion Icons"),
+("LOC_CUI_CONFIG_TOGGLE_CITY_BANNERS", "it_IT", "Mostra/nascondi banner città"),
+("LOC_CUI_CONFIG_OPEN_UNIT_LIST",      "it_IT", "Aprire l'elenco delle unità"),
+("LOC_CUI_CONFIG_OPEN_NOTES",          "it_IT", "Open Notes"),
+
+-- ===========================================================================
+-- [Japanese] by [J4A] 2/17/2019
+-- ===========================================================================
+("LOC_CUI_CONFIG_CATEGORY",            "ja_JP", "Concise UI"),
+("LOC_CUI_CONFIG_PLACE_MAP_PIN",       "ja_JP", "ピンの追加"),
+("LOC_CUI_CONFIG_TOGGLE_IMPROVED",     "ja_JP", "改善済み資源の表示"),
+("LOC_CUI_CONFIG_TOGGLE_UNIT_FLAGS",   "ja_JP", "ユニットアイコンの表示"),
+("LOC_CUI_CONFIG_TOGGLE_TRADERS",      "ja_JP", "交易商アイコンの表示"),
+("LOC_CUI_CONFIG_TOGGLE_RELIGIONS",    "ja_JP", "宗教アイコンの表示"),
+("LOC_CUI_CONFIG_TOGGLE_CITY_BANNERS", "ja_JP", "都市バナーの表示"),
+("LOC_CUI_CONFIG_OPEN_UNIT_LIST",      "ja_JP", "ユニット一覧を開く"),
+("LOC_CUI_CONFIG_OPEN_NOTES",          "ja_JP", "ノートを開く"),
+
+-- ===========================================================================
+-- [Korean] by [firefanda] 10/22/2019
+-- ===========================================================================
+("LOC_CUI_CONFIG_CATEGORY",            "ko_KR", "Concise UI"),
+("LOC_CUI_CONFIG_PLACE_MAP_PIN",       "ko_KR", "맵 트래커 추가"),
+("LOC_CUI_CONFIG_TOGGLE_IMPROVED",     "ko_KR", "향상된 자원 토글"),
+("LOC_CUI_CONFIG_TOGGLE_UNIT_FLAGS",   "ko_KR", "유닛 플래그 토글"),
+("LOC_CUI_CONFIG_TOGGLE_TRADERS",      "ko_KR", "교역로 아이콘 토글"),
+("LOC_CUI_CONFIG_TOGGLE_RELIGIONS",    "ko_KR", "종교 아이콘 토글"),
+("LOC_CUI_CONFIG_TOGGLE_CITY_BANNERS", "ko_KR", "도시 배너 토글"),
+("LOC_CUI_CONFIG_OPEN_UNIT_LIST",      "ko_KR", "유닛 목록 열기"),
+("LOC_CUI_CONFIG_OPEN_NOTES",          "ko_KR", "노트 열기"),
+
+-- ===========================================================================
+-- untranslated
+-- ===========================================================================
+("LOC_CUI_CONFIG_CATEGORY",            "pl_PL", "Concise UI"),
+("LOC_CUI_CONFIG_PLACE_MAP_PIN",       "pl_PL", "Add Map Tack"),
+("LOC_CUI_CONFIG_TOGGLE_IMPROVED",     "pl_PL", "Toggle Improved Resource"),
+("LOC_CUI_CONFIG_TOGGLE_UNIT_FLAGS",   "pl_PL", "Toggle Unit Flags"),
+("LOC_CUI_CONFIG_TOGGLE_TRADERS",      "pl_PL", "Toggle Trader Icons"),
+("LOC_CUI_CONFIG_TOGGLE_RELIGIONS",    "pl_PL", "Toggle Religion Icons"),
+("LOC_CUI_CONFIG_TOGGLE_CITY_BANNERS", "pl_PL", "Toggle City Banners"),
+("LOC_CUI_CONFIG_OPEN_UNIT_LIST",      "pl_PL", "Open Unit List"),
+("LOC_CUI_CONFIG_OPEN_NOTES",          "pl_PL", "Open Notes"),
+
+-- ===========================================================================
+-- untranslated
+-- ===========================================================================
+("LOC_CUI_CONFIG_CATEGORY",            "pt_BR", "Concise UI"),
+("LOC_CUI_CONFIG_PLACE_MAP_PIN",       "pt_BR", "Add Map Tack"),
+("LOC_CUI_CONFIG_TOGGLE_IMPROVED",     "pt_BR", "Toggle Improved Resource"),
+("LOC_CUI_CONFIG_TOGGLE_UNIT_FLAGS",   "pt_BR", "Toggle Unit Flags"),
+("LOC_CUI_CONFIG_TOGGLE_TRADERS",      "pt_BR", "Toggle Trader Icons"),
+("LOC_CUI_CONFIG_TOGGLE_RELIGIONS",    "pt_BR", "Toggle Religion Icons"),
+("LOC_CUI_CONFIG_TOGGLE_CITY_BANNERS", "pt_BR", "Toggle City Banners"),
+("LOC_CUI_CONFIG_OPEN_UNIT_LIST",      "pt_BR", "Open Unit List"),
+("LOC_CUI_CONFIG_OPEN_NOTES",          "pt_BR", "Open Notes"),
+
+-- ===========================================================================
+-- [Russian] by [iMiAMi] 11/21/2019
+-- ===========================================================================
+("LOC_CUI_CONFIG_CATEGORY",            "ru_RU", "Concise UI"),
+("LOC_CUI_CONFIG_PLACE_MAP_PIN",       "ru_RU", "Добавить метку"),
+("LOC_CUI_CONFIG_TOGGLE_IMPROVED",     "ru_RU", "Переключить улучшения ресурсов"),
+("LOC_CUI_CONFIG_TOGGLE_UNIT_FLAGS",   "ru_RU", "Переключить значки юнитов"),
+("LOC_CUI_CONFIG_TOGGLE_TRADERS",      "ru_RU", "Переключить значки торговцев"),
+("LOC_CUI_CONFIG_TOGGLE_RELIGIONS",    "ru_RU", "Переключить значки религий"),
+("LOC_CUI_CONFIG_TOGGLE_CITY_BANNERS", "ru_RU", "Переключить баннеры городов"),
+("LOC_CUI_CONFIG_OPEN_UNIT_LIST",      "ru_RU", "Открыть список юнитов"),
+("LOC_CUI_CONFIG_OPEN_NOTES",          "ru_RU", "Открыть заметки"),
+
+-- ===========================================================================
+-- [Simplified Chinese] by [eudaimonia]
+-- ===========================================================================
+("LOC_CUI_CONFIG_CATEGORY",            "zh_Hans_CN", "Concise UI"),
+("LOC_CUI_CONFIG_PLACE_MAP_PIN",       "zh_Hans_CN", "添加地图钉"),
+("LOC_CUI_CONFIG_TOGGLE_IMPROVED",     "zh_Hans_CN", "开启/关闭已改良资源图标"),
+("LOC_CUI_CONFIG_TOGGLE_UNIT_FLAGS",   "zh_Hans_CN", "开启/关闭单位图标"),
+("LOC_CUI_CONFIG_TOGGLE_TRADERS",      "zh_Hans_CN", "开启/关闭商人图标"),
+("LOC_CUI_CONFIG_TOGGLE_RELIGIONS",    "zh_Hans_CN", "开启/关闭宗教单位图标"),
+("LOC_CUI_CONFIG_TOGGLE_CITY_BANNERS", "zh_Hans_CN", "开启/关闭城市横幅"),
+("LOC_CUI_CONFIG_OPEN_UNIT_LIST",      "zh_Hans_CN", "打开单位列表"),
+("LOC_CUI_CONFIG_OPEN_NOTES",          "zh_Hans_CN", "打开备忘录"),
+
+-- ===========================================================================
+-- untranslated
+-- ===========================================================================
+("LOC_CUI_CONFIG_CATEGORY",            "zh_Hant_HK", "Concise UI"),
+("LOC_CUI_CONFIG_PLACE_MAP_PIN",       "zh_Hant_HK", "添加地图钉"),
+("LOC_CUI_CONFIG_TOGGLE_IMPROVED",     "zh_Hant_HK", "开启/关闭已改良资源图标"),
+("LOC_CUI_CONFIG_TOGGLE_UNIT_FLAGS",   "zh_Hant_HK", "开启/关闭单位图标"),
+("LOC_CUI_CONFIG_TOGGLE_TRADERS",      "zh_Hant_HK", "开启/关闭商人图标"),
+("LOC_CUI_CONFIG_TOGGLE_RELIGIONS",    "zh_Hant_HK", "开启/关闭宗教单位图标"),
+("LOC_CUI_CONFIG_TOGGLE_CITY_BANNERS", "zh_Hant_HK", "开启/关闭城市横幅"),
+("LOC_CUI_CONFIG_OPEN_UNIT_LIST",      "zh_Hant_HK", "打开单位列表"),
+("LOC_CUI_CONFIG_OPEN_NOTES",          "zh_Hant_HK", "打开备忘录");
+
+-- ===========================================================================
+-- EOF
