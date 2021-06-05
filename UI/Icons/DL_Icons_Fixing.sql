41580e9747dd8112776c49ca0fd250c4e899acf6
DeepLogic <>
2021-06-05 09:56:16 +0800

    [dl] fix temple of heaven

--- UI/Icons/DL_Icons_Fixing.sql
+++ UI/Icons/DL_Icons_Fixing.sql
@@ -8,12 +8,12 @@ delete from IconDefinitions where
 --==========================================================================================================================
 INSERT OR REPLACE INTO IconTextureAtlases 
             (Name,                          IconSize,   Filename,                               IconsPerRow,    IconsPerColumn)
-VALUES      ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 256,        'Icon_Temple_Of_Heaven_256.dds',        1,              1),
-            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 128,        'Icon_Temple_Of_Heaven_128.dds',        1,              1),
-            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 64,         'Icon_Temple_Of_Heaven_64.dds',         1,              1),
-            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 50,         'Icon_Temple_Of_Heaven_50.dds',         1,              1),         
-            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 38,         'Icon_Temple_Of_Heaven_38.dds',         1,              1),         
-            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 32,         'Icon_Temple_Of_Heaven_32.dds',         1,              1);
+VALUES      ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 256,        'Icon_Temple_Of_Heaven_256',        1,              1),
+            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 128,        'Icon_Temple_Of_Heaven_128',        1,              1),
+            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 64,         'Icon_Temple_Of_Heaven_64',         1,              1),
+            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 50,         'Icon_Temple_Of_Heaven_50',         1,              1),         
+            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 38,         'Icon_Temple_Of_Heaven_38',         1,              1),         
+            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 32,         'Icon_Temple_Of_Heaven_32',         1,              1);
 --==========================================================================================================================
 -- IconTextureAtlases
 --==========================================================================================================================
@@ -29,12 +29,12 @@ VALUES      ('ICON_BUILDING_TEMPLE_OF_HEAVEN',  'ICON_ATLAS_TEMPLE_OF_HEAVEN',  
 --==========================================================================================================================
 INSERT OR REPLACE INTO IconTextureAtlases 
             (Name,                          IconSize,   Filename,                               IconsPerRow,    IconsPerColumn)
-VALUES      ('ICON_SUK_WAT_ARUN_ATLAS',     256,        'Icon_Suk_Wat_Arun_256.dds',            1,              1),
-            ('ICON_SUK_WAT_ARUN_ATLAS',     128,        'Icon_Suk_Wat_Arun_128.dds',            1,              1),
-            ('ICON_SUK_WAT_ARUN_ATLAS',     64,         'Icon_Suk_Wat_Arun_64.dds',             1,              1),
-            ('ICON_SUK_WAT_ARUN_ATLAS',     50,         'Icon_Suk_Wat_Arun_50.dds',             1,              1),         
-            ('ICON_SUK_WAT_ARUN_ATLAS',     38,         'Icon_Suk_Wat_Arun_38.dds',             1,              1),         
-            ('ICON_SUK_WAT_ARUN_ATLAS',     32,         'Icon_Suk_Wat_Arun_32.dds',             1,              1);
+VALUES      ('ICON_SUK_WAT_ARUN_ATLAS',     256,        'Icon_Suk_Wat_Arun_256',            1,              1),
+            ('ICON_SUK_WAT_ARUN_ATLAS',     128,        'Icon_Suk_Wat_Arun_128',            1,              1),
+            ('ICON_SUK_WAT_ARUN_ATLAS',     64,         'Icon_Suk_Wat_Arun_64',             1,              1),
+            ('ICON_SUK_WAT_ARUN_ATLAS',     50,         'Icon_Suk_Wat_Arun_50',             1,              1),         
+            ('ICON_SUK_WAT_ARUN_ATLAS',     38,         'Icon_Suk_Wat_Arun_38',             1,              1),         
+            ('ICON_SUK_WAT_ARUN_ATLAS',     32,         'Icon_Suk_Wat_Arun_32',             1,              1);
 --==========================================================================================================================
 -- IconTextureAtlases
 --==========================================================================================================================
