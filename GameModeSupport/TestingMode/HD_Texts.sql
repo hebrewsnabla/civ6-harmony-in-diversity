383c4a2c103399e455179453691f652c7cd9b3d2
DeepLogic <>
2021-06-06 15:09:41 +0800

    [dl] texts for the base ability of units

--- GameModeSupport/TestingMode/HD_Texts.sql
+++ GameModeSupport/TestingMode/HD_Texts.sql
@@ -145,3 +145,203 @@ values
     ("zh_Hans_CN",  "LOC_PROMOTION_ARROW_STORM_DESCRIPTION",                                "攻击时+7 [ICON_Strength] 战斗力。"),
     ("zh_Hans_CN",  "LOC_PROMOTION_EMPLACEMENT_DESCRIPTION",                                "防御远程攻击时+10 [ICON_Strength] 战斗力。")
     ;
+
+
+--------------------------------------------------------------------------------
+-- Language: en_US
+-- update EnglishText set Text = Text || '[NEWLINE]TestMessage' where Tag in (select Description from Units where PromotionClass = 'PROMOTION_CLASS_RECON');
+update LocalizedText set Text = Text || '[NEWLINE][NEWLINE]Recon: Faster [ICON_Movement] Movement in Hills, Woods and Rainforest terrain.' where Language = 'en_US' and (
+    Tag = 'LOC_UNIT_SCOUT_DESCRIPTION' or
+    Tag = 'LOC_UNIT_RANGER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_SCOTTISH_HIGHLANDER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_CREE_OKIHTCITAW_DESCRIPTION' or
+    Tag = 'LOC_UNIT_SPEC_OPS_DESCRIPTION' or
+    Tag = 'LOC_UNIT_SKIRMISHER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_INCA_WARAKAQ_DESCRIPTION');
+
+update LocalizedText set Text = Text || '[NEWLINE][NEWLINE]Melee: +5 [ICON_Strength] when attacking Districts.' where Language = 'en_US' and (
+    Tag = 'LOC_UNIT_WARRIOR_DESCRIPTION' or
+    Tag = 'LOC_UNIT_SWORDSMAN_DESCRIPTION' or
+    Tag = 'LOC_UNIT_ROMAN_LEGION_DESCRIPTION' or
+    Tag = 'LOC_UNIT_KONGO_SHIELD_BEARER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_JAPANESE_SAMURAI_DESCRIPTION' or
+    Tag = 'LOC_UNIT_NORWEGIAN_BERSERKER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MUSKETMAN_DESCRIPTION' or
+    Tag = 'LOC_UNIT_SPANISH_CONQUISTADOR_DESCRIPTION' or
+    Tag = 'LOC_UNIT_ENGLISH_REDCOAT_DESCRIPTION' or
+    Tag = 'LOC_UNIT_FRENCH_GARDE_IMPERIALE_DESCRIPTION' or
+    Tag = 'LOC_UNIT_INFANTRY_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MECHANIZED_INFANTRY_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MAN_AT_ARMS_DESCRIPTION' or
+    Tag = 'LOC_UNIT_LINE_INFANTRY_DESCRIPTION' or
+    Tag = 'LOC_UNIT_AZTEC_EAGLE_WARRIOR_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MACEDONIAN_HYPASPIST_DESCRIPTION' or
+    Tag = 'LOC_UNIT_PERSIAN_IMMORTAL_DESCRIPTION' or
+    Tag = 'LOC_UNIT_DIGGER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_GEORGIAN_KHEVSURETI_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MAORI_TOA_DESCRIPTION' or
+    Tag = 'LOC_UNIT_SULEIMAN_JANISSARY_DESCRIPTION' or
+    Tag = 'LOC_UNIT_GAUL_GAESATAE_DESCRIPTION' or
+    Tag = 'LOC_UNIT_BABYLONIAN_SABUM_KIBITTUM_DESCRIPTION');
+
+update LocalizedText set Text = Text || '[NEWLINE][NEWLINE]Anti Cavalry: +10 [ICON_Strength] Combat Strength against Light and Heavy Cavalry.' where Language = 'en_US' and (
+    Tag = 'LOC_UNIT_SPEARMAN_DESCRIPTION' or
+    Tag = 'LOC_UNIT_GREEK_HOPLITE_DESCRIPTION' or
+    Tag = 'LOC_UNIT_PIKEMAN_DESCRIPTION' or
+    Tag = 'LOC_UNIT_AT_CREW_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MODERN_AT_DESCRIPTION' or
+    Tag = 'LOC_UNIT_ZULU_IMPI_DESCRIPTION' or
+    Tag = 'LOC_UNIT_PIKE_AND_SHOT_DESCRIPTION' or
+    Tag = 'LOC_UNIT_SWEDEN_CAROLEAN_DESCRIPTION');
+
+update LocalizedText set Text = Text || '[NEWLINE][NEWLINE]Ranged: -17 [ICON_Strength] Combat Strength when attacking districts or Naval units. +5 [ICON_Strength] Combat Strength when occupying either an improvement that provides [ICON_Strength] Defense Strength or a district. -3 [ICON_Strength] Combat Strength when targets in Rainforest or Woods tiles.' where Language = 'en_US' and (
+    Tag = 'LOC_UNIT_SLINGER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_BARBARIAN_HORSE_ARCHER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_ARCHER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_SCYTHIAN_HORSE_ARCHER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_EGYPTIAN_CHARIOT_ARCHER_EXPANSION2_DESCRIPTION' or
+    Tag = 'LOC_UNIT_CROSSBOWMAN_DESCRIPTION' or
+    Tag = 'LOC_UNIT_CHINESE_CROUCHING_TIGER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_FIELD_CANNON_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MACHINE_GUN_DESCRIPTION' or
+    Tag = 'LOC_UNIT_NUBIAN_PITATI_DESCRIPTION' or
+    Tag = 'LOC_UNIT_KOREAN_HWACHA_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MONGOLIAN_KESHIG_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MAYAN_HULCHE_DESCRIPTION' or
+    Tag = 'LOC_UNIT_VIETNAMESE_VOI_CHIEN_DESCRIPTION');
+
+update LocalizedText set Text = Text || '[NEWLINE][NEWLINE]Heavy Cavalry: +1 [ICON_Movement] Movement if starting in flat tiles. -5 [ICON_Strength] Combat Strength when attacking districts.' where Language = 'en_US' and (
+    Tag = 'LOC_UNIT_SUMERIAN_WAR_CART_DESCRIPTION' or
+    Tag = 'LOC_UNIT_HEAVY_CHARIOT_DESCRIPTION' or
+    Tag = 'LOC_UNIT_KNIGHT_DESCRIPTION' or
+    Tag = 'LOC_UNIT_INDIAN_VARU_DESCRIPTION' or
+    Tag = 'LOC_UNIT_ARABIAN_MAMLUK_DESCRIPTION' or
+    Tag = 'LOC_UNIT_TANK_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MODERN_ARMOR_DESCRIPTION' or
+    Tag = 'LOC_UNIT_POLISH_HUSSAR_EXPANSION2_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MACEDONIAN_HETAIROI_DESCRIPTION' or
+    Tag = 'LOC_UNIT_CUIRASSIER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MALI_MANDEKALU_CAVALRY_DESCRIPTION' or
+    Tag = 'LOC_UNIT_AMERICAN_ROUGH_RIDER_EXPANSION2_DESCRIPTION' or
+    Tag = 'LOC_UNIT_BYZANTINE_TAGMA_DESCRIPTION');
+
+update LocalizedText set Text = Text || '[NEWLINE][NEWLINE]Light Cavalry: Pillaging costs only 1 [ICON_Movement] Movement point. -7 [ICON_Strength] Combat Strength when attacking districts.' where Language = 'en_US' and (
+    Tag = 'LOC_UNIT_BARBARIAN_HORSEMAN_DESCRIPTION' or
+    Tag = 'LOC_UNIT_HORSEMAN_DESCRIPTION' or
+    Tag = 'LOC_UNIT_CAVALRY_DESCRIPTION' or
+    Tag = 'LOC_UNIT_RUSSIAN_COSSACK_DESCRIPTION' or
+    Tag = 'LOC_UNIT_HELICOPTER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MAPUCHE_MALON_RAIDER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_COURSER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_CANADA_MOUNTIE_DESCRIPTION' or
+    Tag = 'LOC_UNIT_HUNGARY_BLACK_ARMY_DESCRIPTION' or
+    Tag = 'LOC_UNIT_HUNGARY_HUSZAR_DESCRIPTION' or
+    Tag = 'LOC_UNIT_COLOMBIAN_LLANERO_DESCRIPTION' or
+    Tag = 'LOC_UNIT_ETHIOPIAN_OROMO_CAVALRY_EXPANSION2_DESCRIPTION');
+
+update LocalizedText set Text = Text || '[NEWLINE][NEWLINE]Siege: +1 [ICON_Movement] Movement when adjacent to Light or Heavy Cavalry.' where Language = 'en_US' and (
+    Tag = 'LOC_UNIT_CATAPULT_DESCRIPTION' or
+    Tag = 'LOC_UNIT_BOMBARD_DESCRIPTION' or
+    Tag = 'LOC_UNIT_ARTILLERY_DESCRIPTION' or
+    Tag = 'LOC_UNIT_ROCKET_ARTILLERY_DESCRIPTION' or
+    Tag = 'LOC_UNIT_TREBUCHET_DESCRIPTION' or
+    Tag = 'LOC_UNIT_KHMER_DOMREY_DESCRIPTION');
+
+--------------------------------------------------------------------------------
+-- Language: zh_Hans_CN
+update LocalizedText set Text = Text || '[NEWLINE][NEWLINE]侦察：在丘陵、树林和雨林地形有更快的 [ICON_Movement] 移动力。' where Language = 'zh_Hans_CN' and (
+    Tag = 'LOC_UNIT_SCOUT_DESCRIPTION' or
+    Tag = 'LOC_UNIT_RANGER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_SCOTTISH_HIGHLANDER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_CREE_OKIHTCITAW_DESCRIPTION' or
+    Tag = 'LOC_UNIT_SPEC_OPS_DESCRIPTION' or
+    Tag = 'LOC_UNIT_SKIRMISHER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_INCA_WARAKAQ_DESCRIPTION');
+
+update LocalizedText set Text = Text || '[NEWLINE][NEWLINE]近战：攻击区域防御时+5 [ICON_Strength] 战斗力。' where Language = 'zh_Hans_CN' and (
+    Tag = 'LOC_UNIT_WARRIOR_DESCRIPTION' or
+    Tag = 'LOC_UNIT_SWORDSMAN_DESCRIPTION' or
+    Tag = 'LOC_UNIT_ROMAN_LEGION_DESCRIPTION' or
+    Tag = 'LOC_UNIT_KONGO_SHIELD_BEARER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_JAPANESE_SAMURAI_DESCRIPTION' or
+    Tag = 'LOC_UNIT_NORWEGIAN_BERSERKER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MUSKETMAN_DESCRIPTION' or
+    Tag = 'LOC_UNIT_SPANISH_CONQUISTADOR_DESCRIPTION' or
+    Tag = 'LOC_UNIT_ENGLISH_REDCOAT_DESCRIPTION' or
+    Tag = 'LOC_UNIT_FRENCH_GARDE_IMPERIALE_DESCRIPTION' or
+    Tag = 'LOC_UNIT_INFANTRY_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MECHANIZED_INFANTRY_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MAN_AT_ARMS_DESCRIPTION' or
+    Tag = 'LOC_UNIT_LINE_INFANTRY_DESCRIPTION' or
+    Tag = 'LOC_UNIT_AZTEC_EAGLE_WARRIOR_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MACEDONIAN_HYPASPIST_DESCRIPTION' or
+    Tag = 'LOC_UNIT_PERSIAN_IMMORTAL_DESCRIPTION' or
+    Tag = 'LOC_UNIT_DIGGER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_GEORGIAN_KHEVSURETI_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MAORI_TOA_DESCRIPTION' or
+    Tag = 'LOC_UNIT_SULEIMAN_JANISSARY_DESCRIPTION' or
+    Tag = 'LOC_UNIT_GAUL_GAESATAE_DESCRIPTION' or
+    Tag = 'LOC_UNIT_BABYLONIAN_SABUM_KIBITTUM_DESCRIPTION');
+
+update LocalizedText set Text = Text || '[NEWLINE][NEWLINE]抗骑兵：与轻重骑兵作战+10 [ICON_Strength] 战斗力。' where Language = 'zh_Hans_CN' and (
+    Tag = 'LOC_UNIT_SPEARMAN_DESCRIPTION' or
+    Tag = 'LOC_UNIT_GREEK_HOPLITE_DESCRIPTION' or
+    Tag = 'LOC_UNIT_PIKEMAN_DESCRIPTION' or
+    Tag = 'LOC_UNIT_AT_CREW_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MODERN_AT_DESCRIPTION' or
+    Tag = 'LOC_UNIT_ZULU_IMPI_DESCRIPTION' or
+    Tag = 'LOC_UNIT_PIKE_AND_SHOT_DESCRIPTION' or
+    Tag = 'LOC_UNIT_SWEDEN_CAROLEAN_DESCRIPTION');
+
+update LocalizedText set Text = Text || '[NEWLINE][NEWLINE]远程：攻击区域防御和海军单位时-17 [ICON_Strength] 战斗力。占领一个区域或一个提供 [ICON_Strength] 防御力的改良设施时+5 [ICON_Strength] 战斗力。攻击森林或雨林中的目标时-3 [ICON_Strength] 战斗力。' where Language = 'zh_Hans_CN' and (
+    Tag = 'LOC_UNIT_SLINGER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_BARBARIAN_HORSE_ARCHER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_ARCHER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_SCYTHIAN_HORSE_ARCHER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_EGYPTIAN_CHARIOT_ARCHER_EXPANSION2_DESCRIPTION' or
+    Tag = 'LOC_UNIT_CROSSBOWMAN_DESCRIPTION' or
+    Tag = 'LOC_UNIT_CHINESE_CROUCHING_TIGER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_FIELD_CANNON_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MACHINE_GUN_DESCRIPTION' or
+    Tag = 'LOC_UNIT_NUBIAN_PITATI_DESCRIPTION' or
+    Tag = 'LOC_UNIT_KOREAN_HWACHA_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MONGOLIAN_KESHIG_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MAYAN_HULCHE_DESCRIPTION' or
+    Tag = 'LOC_UNIT_VIETNAMESE_VOI_CHIEN_DESCRIPTION');
+
+update LocalizedText set Text = Text || '[NEWLINE][NEWLINE]重骑兵：如果在平坦地形开始回合，本回合+1 [ICON_Movement] 移动力。攻击区域防御时-5 [ICON_Strength] 战斗力。' where Language = 'zh_Hans_CN' and (
+    Tag = 'LOC_UNIT_SUMERIAN_WAR_CART_DESCRIPTION' or
+    Tag = 'LOC_UNIT_HEAVY_CHARIOT_DESCRIPTION' or
+    Tag = 'LOC_UNIT_KNIGHT_DESCRIPTION' or
+    Tag = 'LOC_UNIT_INDIAN_VARU_DESCRIPTION' or
+    Tag = 'LOC_UNIT_ARABIAN_MAMLUK_DESCRIPTION' or
+    Tag = 'LOC_UNIT_TANK_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MODERN_ARMOR_DESCRIPTION' or
+    Tag = 'LOC_UNIT_POLISH_HUSSAR_EXPANSION2_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MACEDONIAN_HETAIROI_DESCRIPTION' or
+    Tag = 'LOC_UNIT_CUIRASSIER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MALI_MANDEKALU_CAVALRY_DESCRIPTION' or
+    Tag = 'LOC_UNIT_AMERICAN_ROUGH_RIDER_EXPANSION2_DESCRIPTION' or
+    Tag = 'LOC_UNIT_BYZANTINE_TAGMA_DESCRIPTION');
+
+update LocalizedText set Text = Text || '[NEWLINE][NEWLINE]轻骑兵：劫掠只消耗一点 [ICON_Movement] 移动力。攻击区域防御时-7 [ICON_Strength] 战斗力。' where Language = 'zh_Hans_CN' and (
+    Tag = 'LOC_UNIT_BARBARIAN_HORSEMAN_DESCRIPTION' or
+    Tag = 'LOC_UNIT_HORSEMAN_DESCRIPTION' or
+    Tag = 'LOC_UNIT_CAVALRY_DESCRIPTION' or
+    Tag = 'LOC_UNIT_RUSSIAN_COSSACK_DESCRIPTION' or
+    Tag = 'LOC_UNIT_HELICOPTER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_MAPUCHE_MALON_RAIDER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_COURSER_DESCRIPTION' or
+    Tag = 'LOC_UNIT_CANADA_MOUNTIE_DESCRIPTION' or
+    Tag = 'LOC_UNIT_HUNGARY_BLACK_ARMY_DESCRIPTION' or
+    Tag = 'LOC_UNIT_HUNGARY_HUSZAR_DESCRIPTION' or
+    Tag = 'LOC_UNIT_COLOMBIAN_LLANERO_DESCRIPTION' or
+    Tag = 'LOC_UNIT_ETHIOPIAN_OROMO_CAVALRY_EXPANSION2_DESCRIPTION');
+
+update LocalizedText set Text = Text || '[NEWLINE][NEWLINE]攻城：若相邻轻重骑兵，则+1 [ICON_Movement] 移动力。' where Language = 'zh_Hans_CN' and (
+    Tag = 'LOC_UNIT_CATAPULT_DESCRIPTION' or
+    Tag = 'LOC_UNIT_BOMBARD_DESCRIPTION' or
+    Tag = 'LOC_UNIT_ARTILLERY_DESCRIPTION' or
+    Tag = 'LOC_UNIT_ROCKET_ARTILLERY_DESCRIPTION' or
+    Tag = 'LOC_UNIT_TREBUCHET_DESCRIPTION' or
+    Tag = 'LOC_UNIT_KHMER_DOMREY_DESCRIPTION');
