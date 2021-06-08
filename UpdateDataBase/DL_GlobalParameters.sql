2bab7deabeaa122875ccecfddecdab231aff0cdf
DeepLogic <>
2021-06-08 16:53:01 +0800

    [v0.7.9.2] add icon for city center buildings

--- UpdateDataBase/DL_GlobalParameters.sql
+++ UpdateDataBase/DL_GlobalParameters.sql
@@ -26,6 +26,7 @@ update Eras_XP2 set TradeRouteMinimumEndTurnChange = 0;
 -- Start from Industrial Era.
 update GlobalParameters set Value = 4 where Name = 'WORLD_CONGRESS_INITIAL_ERA';
 update GlobalParameters set Value = 15 where Name = 'WORLD_CONGRESS_MAX_TIME_BETWEEN_MEETINGS';
+update GlobalParameters set Value = 15 where Name = 'WORLD_CONGRESS_MIN_TIME_BETWEEN_SPECIAL_SESSIONS';
 
 update GlobalParameters set Value = 15 where Name = 'DIPLOMACY_ALLIANCE_TIME_LIMIT';
 update GlobalParameters set Value = 15 where Name = 'DIPLOMACY_DECLARED_FRIENDSHIP_TIME_LIMIT';
