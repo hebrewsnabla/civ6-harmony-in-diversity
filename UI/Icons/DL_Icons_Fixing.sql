b142de70c7036d31016d782ee4e8ee55594588fb
DeepLogic <>
2021-06-11 22:27:07 +0800

    [dl] fix icon blur issue for two mod wonders, add authors to modinfo

--- UI/Icons/DL_Icons_Fixing.sql
+++ UI/Icons/DL_Icons_Fixing.sql
@@ -7,13 +7,13 @@ delete from IconDefinitions where
 -- IconTextureAtlases
 --==========================================================================================================================
 INSERT OR REPLACE INTO IconTextureAtlases 
-            (Name,                          IconSize,   Filename,                               IconsPerRow,    IconsPerColumn)
-VALUES      ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 256,        'Icon_Temple_Of_Heaven_256',        1,              1),
-            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 128,        'Icon_Temple_Of_Heaven_128',        1,              1),
-            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 64,         'Icon_Temple_Of_Heaven_64',         1,              1),
-            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 50,         'Icon_Temple_Of_Heaven_50',         1,              1),         
-            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 38,         'Icon_Temple_Of_Heaven_38',         1,              1),         
-            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 32,         'Icon_Temple_Of_Heaven_32',         1,              1);
+            (Name,                          IconSize,   Filename,                           IconsPerRow,    IconsPerColumn)
+VALUES      ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 32,         'Icon_Temple_Of_Heaven_32',         1,              1),
+            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 38,         'Icon_Temple_Of_Heaven_38',         1,              1),
+            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 50,         'Icon_Temple_Of_Heaven_50',         1,              1),
+            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 64,         'Icon_Temple_Of_Heaven_64',         1,              1),         
+            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 128,        'Icon_Temple_Of_Heaven_128',        1,              1),         
+            ('ICON_ATLAS_TEMPLE_OF_HEAVEN', 256,        'Icon_Temple_Of_Heaven_256',        1,              1);
 --==========================================================================================================================
 -- IconTextureAtlases
 --==========================================================================================================================
@@ -28,13 +28,13 @@ VALUES      ('ICON_BUILDING_TEMPLE_OF_HEAVEN',  'ICON_ATLAS_TEMPLE_OF_HEAVEN',  
 -- IconTextureAtlases
 --==========================================================================================================================
 INSERT OR REPLACE INTO IconTextureAtlases 
-            (Name,                          IconSize,   Filename,                               IconsPerRow,    IconsPerColumn)
-VALUES      ('ICON_SUK_WAT_ARUN_ATLAS',     256,        'Icon_Suk_Wat_Arun_256',            1,              1),
-            ('ICON_SUK_WAT_ARUN_ATLAS',     128,        'Icon_Suk_Wat_Arun_128',            1,              1),
-            ('ICON_SUK_WAT_ARUN_ATLAS',     64,         'Icon_Suk_Wat_Arun_64',             1,              1),
-            ('ICON_SUK_WAT_ARUN_ATLAS',     50,         'Icon_Suk_Wat_Arun_50',             1,              1),         
-            ('ICON_SUK_WAT_ARUN_ATLAS',     38,         'Icon_Suk_Wat_Arun_38',             1,              1),         
-            ('ICON_SUK_WAT_ARUN_ATLAS',     32,         'Icon_Suk_Wat_Arun_32',             1,              1);
+            (Name,                          IconSize,   Filename,                           IconsPerRow,    IconsPerColumn)
+VALUES      ('ICON_SUK_WAT_ARUN_ATLAS',     32,         'Icon_Suk_Wat_Arun_32',             1,              1),
+            ('ICON_SUK_WAT_ARUN_ATLAS',     38,         'Icon_Suk_Wat_Arun_38',             1,              1),
+            ('ICON_SUK_WAT_ARUN_ATLAS',     50,         'Icon_Suk_Wat_Arun_50',             1,              1),
+            ('ICON_SUK_WAT_ARUN_ATLAS',     64,         'Icon_Suk_Wat_Arun_64',             1,              1),         
+            ('ICON_SUK_WAT_ARUN_ATLAS',     128,        'Icon_Suk_Wat_Arun_128',            1,              1),         
+            ('ICON_SUK_WAT_ARUN_ATLAS',     256,        'Icon_Suk_Wat_Arun_256',            1,              1);
 --==========================================================================================================================
 -- IconTextureAtlases
 --==========================================================================================================================
