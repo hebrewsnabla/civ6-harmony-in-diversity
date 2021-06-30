df3bab69909be8c872658da1e956338e7374464f
DeepLogic <>
2021-06-30 15:41:48 +0800

    [v0.8.4.1] add support for configure minimum city distance

---
+++ ModSupport/GeneralChineseSupport/Texts.sql
@@ -0,0 +1,27 @@
+--------------------------------------------------------------------------------
+-- Language: zh_Hans_CN
+insert or replace into LocalizedText
+    (Language,      Tag,                                Text)
+values
+    -- Configure Minimal City Distance
+    ("zh_Hans_CN",  "LOC_CMCD_NAME",                    "最小城市间距"),
+    ("zh_Hans_CN",  "LOC_CMCD_DESCRIPTION",             "建立城市时的最近距离限制"),
+    ("zh_Hans_CN",  "LOC_CMCD_HEADER_NAME",             "[COLOR_Production]设置最小城市间距[ENDCOLOR]"),
+    ("zh_Hans_CN",  "LOC_CMCD_HEADER_DESCRIPTION",      "由DahakaMVl制作的 设置最小城市间距"),
+    ("zh_Hans_CN",  "LOC_CMCD_FOOTER_NAME",             "[COLOR_Production]设置最小城市间距 结束[ENDCOLOR]"),
+    ("zh_Hans_CN",  "LOC_CMCD_FOOTER_DESCRIPTION",      "用于填充的假选项"),
+    -- RadialMeasuringTool
+    ("zh_Hans_CN",  "LOC_RMT_WND_TITLE",                "半径测量工具"),
+    ("zh_Hans_CN",  "LOC_RMT_LBL1",                     "点击地图上的位置来可视化半径内的地格"),
+    ("zh_Hans_CN",  "LOC_RMT_RAD_LBL",                  "半径:"),
+    ("zh_Hans_CN",  "LOC_RMT_ALT_RAD_PREFIX",           "在"),
+    ("zh_Hans_CN",  "LOC_RMT_ALT_RAD_SUFIX",            "格内"),
+    ("zh_Hans_CN",  "LOC_RMT_ALT_RAD_CB_TT",            "选中后采用在x格内的计数方式"),
+    ("zh_Hans_CN",  "LOC_RMT_CB_MULTI",                 "多重半径高亮"),
+    ("zh_Hans_CN",  "LOC_RMT_CB_CITY_ONLY",             "只高亮市中心"),
+    ("zh_Hans_CN",  "LOC_RMT_CB_BONUS_RES",             "忽略加成资源"),
+    ("zh_Hans_CN",  "LOC_RMT_MAX_CITY_RAD",             "- 市民可工作范围"),
+    ("zh_Hans_CN",  "LOC_RMT_KEY_CITY_CEN",             "市中心"),
+    ("zh_Hans_CN",  "LOC_RMT_KEY_RES",                  "资源"),
+    ("zh_Hans_CN",  "LOC_RMT_CB_TRANSP",                "使用透明的窗口背景"),
+    ("zh_Hans_CN",  "LOC_RMT_CB_WND_POS",               "窗口位置");
