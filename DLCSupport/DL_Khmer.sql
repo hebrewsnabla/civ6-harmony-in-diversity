a7bd576fdbf87d0ed0fe1c7c459e386b845ba865
zhushou <582372326@qq.com>
2021-06-30 17:13:44 +0800

    Kampung to be tested

--- DLCSupport/DL_Khmer.sql
+++ DLCSupport/DL_Khmer.sql
@@ -21,3 +21,21 @@ update ModifierArguments set Value = 1 where ModifierId = 'TRAIT_NUSANTARA_COAST
 
 insert or replace into Improvement_ValidFeatures (ImprovementType, FeatureType, PrereqTech, PrereqCivic) 
 values  ('IMPROVEMENT_KAMPUNG', 'FEATURE_REEF', NULL, NULL);
+
+update Improvement_YieldChanges set YieldChange = 1 where ImprovementType = 'IMPROVEMENT_KAMPUNG' and YieldType = 'YIELD_FOOD';
+update Adjacency_YieldChanges set YieldType = 'YIELD_PRODUCTION' where ID = 'Kampung_FoodFishingBoats';
+update Improvement_Tourism set TourismSource = 'YIELD_PRODUCTION' where ImprovementType = 'IMPROVEMENT_KAMPUNG';
+update RequirementSetRequirements set RequirementId = 'REQUIRES_PLAYER_HAS_CIVIC_CIVIL_ENGINEERING' 
+where RequirementSetId = 'PLAYER_HAS_KAMPUNG_TECHNOLOGY_REQUIREMENTS' and RequirementId = 'REQUIRES_PLAYER_HAS_MASS_PRODUCTION_TECHNOLOGY';
+
+insert or replace into Improvement_Adjacencies
+    (ImprovementType,               YieldChangeId)
+values
+    ('IMPROVEMENT_KAMPUNG',         'Kampung_ProductionCityCenter'),
+    ('IMPROVEMENT_FISHING_BOATS',   'Fishingboats_Kampung_Production');
+
+insert or replace into Adjacency_YieldChanges
+	(ID,				                Description,	YieldType,			YieldChange,	TilesRequired,  AdjacentImprovement,    AdjacentDistrict, 		    PrereqCivic)
+values
+    ('Kampung_ProductionCityCenter',	'Placeholder', 'YIELD_PRODUCTION',  1,				1,              NULL,                   'DISTRICT_CITY_CENTER', 	NULL),
+	('Fishingboats_Kampung_Production',	'Placeholder', 'YIELD_PRODUCTION',	1,				2,              'IMPROVEMENT_KAMPUNG',  NULL,                       'CIVIC_CIVIL_ENGINEERING');
