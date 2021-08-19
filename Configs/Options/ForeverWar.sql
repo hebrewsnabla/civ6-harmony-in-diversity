b885e6b7e60cde995766a91bb901e73f1cad3336
DeepLogic <>
2021-08-19 14:37:02 +0800

    [v1.0.1.3] add icons for barbarian horseman (thanks eps tweak)

--- Configs/Options/ForeverWar.sql
+++ Configs/Options/ForeverWar.sql
@@ -1,3 +1,3 @@
 update GlobalParameters set Value = 1 where Name = 'DIPLOMACY_WAR_LAST_FOREVER';
 -- update GlobalParameters set Value = 0 where Name = 'WAR_WEARINESS_WARMONGER_BASE';
-update GlobalParameters set Value = 10000 where Name = 'WAR_WEARINESS_POINTS_FOR_AMENITY_LOSS';
+update GlobalParameters set Value = 4000 where Name = 'WAR_WEARINESS_POINTS_FOR_AMENITY_LOSS';
