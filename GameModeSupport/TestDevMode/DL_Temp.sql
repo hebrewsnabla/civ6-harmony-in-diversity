2928fa1f6bd4ee3a4d824d2441969de6ccb6943e
DeepLogic <>
2021-08-27 23:05:13 +0800

    [v1.0.3] version

--- GameModeSupport/TestDevMode/DL_Temp.sql
+++ GameModeSupport/TestDevMode/DL_Temp.sql
@@ -1,3 +1,6 @@
 -------------------------------------
 --     Temp Testing Adjustments    --
 -------------------------------------
+
+update UnitOperations set BaseProbability = 14 where OperationType = 'UNITOPERATION_SPY_SIPHON_FUNDS';
+update UnitOperations set BaseProbability = 16 where OperationType = 'UNITOPERATION_SPY_NEUTRALIZE_GOVERNOR';
