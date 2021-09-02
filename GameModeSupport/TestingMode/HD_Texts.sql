ccdb49640fb9df58f988c1c073235abf11745311
zhushou <582372326@qq.com>
2021-09-02 14:01:04 +0800

    text

--- GameModeSupport/TestingMode/HD_Texts.sql
+++ GameModeSupport/TestingMode/HD_Texts.sql
@@ -3,7 +3,7 @@
 insert or replace into EnglishText
     (Tag,                                                                       Text)
 values
-    ("LOC_ABILITY_LEVY_DEBUFF_IN_ENEMY_TERRITORY_DESCRIPTION",                  "-5 [ICON_Strength] Combat Strength in enemy's territory."),
+    ("LOC_ABILITY_LEVY_DEBUFF_IN_ENEMY_TERRITORY_DESCRIPTION",                  "-4 [ICON_Strength] Combat Strength in enemy's territory."),
     ("LOC_LEVY_DEBUFF_IN_ENEMY_TERRITORY_PREVIEW_TEXT",                         "in enemy's territory"),
     ("LOC_BUILDING_BASILIKOI_PAIDES_EXPANSION2_DESCRIPTION",                    "A building unique to Macedon. +25% combat experience for all land units trained in this city. Gain [ICON_SCIENCE] Science equal to 25% of the unit's cost when a non civilian unit is created in this city.[NEWLINE][NEWLINE]May not be built in an Encampment district that already has a Stable.[NEWLINE][NEWLINE]+2 [ICON_PRODUCTION] Production to each improved [ICON_RESOURCE_IRON] Iron in this city. +3 [ICON_STRENGTH] Combat Strength for all land units trained in this city."),
     ("LOC_BUILDING_GOV_CONQUEST_DESCRIPTION",                                   "Capturing an enemy City grants 15% bonus [ICON_Production] Production in all Cities for 5 turns.[NEWLINE]DouBle the [ICON_Gold] Gold gained when capturing a city. [NEWLINE]+100% yields gained from pilaging and coastal raids.[NEWLINE]Awards +1 [ICON_Governor] Governor Title.");
@@ -13,7 +13,7 @@ values
 insert or replace into LocalizedText
     (Language,      Tag,                                                                    Text)
 values
-    ("zh_Hans_CN",  "LOC_ABILITY_LEVY_DEBUFF_IN_ENEMY_TERRITORY_DESCRIPTION",               "位于敌方领土时-5 [ICON_Strength] 战斗力"),
+    ("zh_Hans_CN",  "LOC_ABILITY_LEVY_DEBUFF_IN_ENEMY_TERRITORY_DESCRIPTION",               "位于敌方领土时-4 [ICON_Strength] 战斗力"),
     ("zh_Hans_CN",  "LOC_LEVY_DEBUFF_IN_ENEMY_TERRITORY_PREVIEW_TEXT",                      "位于敌方领土"),
     ("zh_Hans_CN",  "LOC_BUILDING_BASILIKOI_PAIDES_EXPANSION2_DESCRIPTION",                 "马其顿特色建筑。此城中训练的所有陆地单位+25%战斗经验值。非平民单位在此城中创建时，获得等同于单位花费25%的 [ICON_SCIENCE] 科技值。[NEWLINE][NEWLINE]无法建造在已拥有马厩的军营区域中。[NEWLINE][NEWLINE]本城的所有已开发的 [ICON_RESOURCE_IRON] 铁+2 [ICON_Production] 生产力。为在本城训练的陆地单位提供永久+3 [ICON_Strength] 战斗力。"),
     ("zh_Hans_CN",  "LOC_BUILDING_GOV_CONQUEST_DESCRIPTION",                                "占领一座敌方城市后，所有城市将获得15%的 [ICON_Production] 生产力加成，效果持续5回合。[NEWLINE]占领城市时获得的 [ICON_Gold] 金币数额翻倍。[NEWLINE]掠夺和海岸扫荡的收益+100%。[NEWLINE]奖励1个 [ICON_Governor] 总督头衔。");
