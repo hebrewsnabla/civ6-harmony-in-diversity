3e4a64efa687b7b26bd94f5f5ff89111832a0825
DeepLogic <>
2021-09-23 21:03:40 +0800

    [dl] some text fixing

--- SubMods/BetterChinese/Text/BetterChineseAddon_Text.sql
+++ SubMods/BetterChinese/Text/BetterChineseAddon_Text.sql
@@ -3,6 +3,8 @@
 insert or replace into LocalizedText
     (Language,      Tag,                                                                 Text)
 values
+    ("zh_Hans_CN",  "LOC_TOOLTIP_PLACEMENT_REQUIRES_COAST",                              "必须建在湖泊或近海单元格上。"),
+    ("zh_Hans_CN",  "LOC_UI_PEDIA_PLACEMENT_REQUIRES_COAST",                             "必须建在湖泊或近海单元格上。"),
     ("zh_Hans_CN",  "LOC_LEADER_TRAIT_JOHANNESBURG_DESCRIPTION",                         "每种改良的资源为城市提供+1 [ICON_Production] 生产力。研究“工业化”之后提升为+2 [ICON_Production] 生产力。"),
     ("zh_Hans_CN",  "LOC_CIVILIZATION_JOHANNESBURG_BONUS",                               "每种改良的资源为城市提供+1 [ICON_Production] 生产力。研究“工业化”之后提升为+2 [ICON_Production] 生产力。"),
     ("zh_Hans_CN",  "LOC_IMPROVEMENT_CHATEAU_NAME",                                      "城堡庄园");
