42904d304543f69cf63f1927e6a9d82f981909c8
DeepLogic <>
2021-10-18 23:43:33 +0800

    [dl] move the monopoly game mode contents to standalone mod

--- GameModeSupport/MonopoliesMode/HD_FrontEnd_Text.sql
+++ GameModeSupport/MonopoliesMode/HD_FrontEnd_Text.sql
@@ -4,7 +4,7 @@ insert or replace into EnglishText
     (Tag,                                                             Text)
 values
     ("LOC_GAMEMODE_MONOPOLIES_NAME",                                  "Industries and Corporations Mode"),
-    ("LOC_GAMEMODE_MONOPOLIES_DESCRIPTION",                           "Control, improve, and expand upon your Luxury resources! The Industries and Corporations mode enables you to build profitable Industries around Luxury resources, found Corporations to further their development, and manufacture unique Products to secure your place in business history.");
+    ("LOC_GAMEMODE_MONOPOLIES_DESCRIPTION",                           "[COLOR_Red]Please Subscribe ""Harmony in Diversity"": Industries and Corporations Mode (standalone MOD), We have overhaul the whole game mode, including redesigning the benefits of Industries, Corporations and Products.[ENDCOLOR][NEWLINE]Control, improve, and expand upon your Luxury resources! The Industries and Corporations mode enables you to build profitable Industries around Luxury resources, found Corporations to further their development, and manufacture unique Products to secure your place in business history.");
 
 --------------------------------------------------------------------------------
 -- Language: zh_Hans_CN
@@ -12,4 +12,4 @@ insert or replace into LocalizedText
     (Language,      Tag,                                                             Text)
 values
     ("zh_Hans_CN",  "LOC_GAMEMODE_MONOPOLIES_NAME",                                  "行业与公司模式"),
-    ("zh_Hans_CN",  "LOC_GAMEMODE_MONOPOLIES_DESCRIPTION",                           "控制、改良您的奢侈品资源，并在此之上进行拓展！在“行业与公司”模式中，您可以围绕奢侈品资源建立利润丰硕的行业、创建公司进行深度开发，并生产特色产品，确立您在商业历史中的显著地位。");
+    ("zh_Hans_CN",  "LOC_GAMEMODE_MONOPOLIES_DESCRIPTION",                           "[COLOR_Red]请订阅并启用“和而不同”行业与公司模式独立MOD，我们对该模式进行了全面的重制，包括行业、公司和产品的效果。[ENDCOLOR][NEWLINE]控制、改良您的奢侈品资源，并在此之上进行拓展！在“行业与公司”模式中，您可以围绕奢侈品资源建立利润丰硕的行业、创建公司进行深度开发，并生产特色产品，确立您在商业历史中的显著地位。");
