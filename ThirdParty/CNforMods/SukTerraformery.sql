4eed3cdbb1574e266028c0badf8a6dca581bdf55
DeepLogic <>
2021-10-26 21:32:51 +0800

    CNforMods included for Resources mods and Suk's Natural Wonders

---
+++ ThirdParty/CNforMods/SukTerraformery.sql
@@ -0,0 +1,35 @@
+--------------------------------------------------------------------------------
+-- Language: zh_Hans_CN
+insert or replace into LocalizedText
+    (Language,      Tag,                                                                         Text)
+values
+    -- 大峡谷
+    ("zh_Hans_CN",  "LOC_FEATURE_SUK_GRANDCANYON_NAME",                                          "亚利桑那大峡谷"),
+    ("zh_Hans_CN",  "LOC_FEATURE_SUK_GRANDCANYON_DESCRIPTION",                                   "占3个单元格无法逾越的自然奇观。可作为淡水源，并为相邻单元格提供+1 [ICON_Food] 食物、+1 [ICON_Culture] 文化值。在对局进行到工业时代之后，城市每拥有一格该奇观，便可获得+4 [ICON_Tourism] 旅游业绩（最多+12）。"),
+    ("zh_Hans_CN",  "LOC_FEATURE_SUK_GRANDCANYON_QUOTE",                                         "亚利桑那州有一座举世无二的伟大奇观，就在这科罗拉多大峡谷之中。你不能去改良它，多少世纪的时光塑造它如今的瑰丽，而人类的“改良”只会适得其反。[NEWLINE]——西奥多·罗斯福"),
+    ("zh_Hans_CN",  "LOC_PEDIA_FEATURES_PAGE_FEATURE_SUK_GRANDCANYON_CHAPTER_HISTORY_PARA_1",    "亚利桑那大峡谷是地球上最壮观的地质景观之一。它的浩瀚令人惊叹，它揭示出的有关地球历史的证据也是无价的。从前寒武纪到新生代，它的地质记录跨越了地球演化史整整四个时代。[NEWLINE]这条1.5公里（0.9英里）深的峡谷蜿蜒曲折，全长445公里（276.5英里），在长达600万年的地质活动和科罗拉多河的侵蚀下逐渐形成。大峡谷以其独特的自然美景而闻名，被认为是世界上最具视觉冲击力的景观之一，它那陡峭的深谷、庙宇般的山丘以及广阔多彩又蜿蜒的地形令人赞不绝口。大峡谷公园内的景观包括高原、平原、沙漠、森林、火山渣锥、熔岩流、溪流、瀑布，以及美国最大的白浪急流之一。随着河流的深入，它在峡谷峭壁之内描绘出北美七大生物地域中的五个区域，演化出不同海拔高度生物环境的演化历程，是一个杰出的范例样本。动植物种群在许多区域交叠融合，且遍及整个峡谷。[NEWLINE]数千年来，一直有印第安人定居于此，他们在峡谷及其众多洞穴内建造住处。普韦布洛人认为大峡谷是一个圣地，并向它进行朝圣。1908年1月11日，由美国总统西奥多·罗斯福（Theodore Roosevelt）提议，在大峡谷建立了国家纪念公园，如今，大峡谷国家公园是世界上最著名的自然景点之一，每年吸引约500万名游客。"),
+    -- 富士山
+    ("zh_Hans_CN",  "LOC_FEATURE_SUK_FUJI_NAME",                                                 "富士山"),
+    ("zh_Hans_CN",  "LOC_FEATURE_SUK_FUJI_DESCRIPTION",                                          "占一个单元格无法逾越的自然奇观。以山脉形式出现，相邻区域可提供+2对应类型的 [ICON_GreatPerson] 伟人点数。"),
+    ("zh_Hans_CN",  "LOC_FEATURE_SUK_FUJI_QUOTE",                                                "一寸寸，[NEWLINE]小蜗牛，[NEWLINE]爬向富士山！[NEWLINE]——小林一茶"),
+    ("zh_Hans_CN",  "LOC_PEDIA_FEATURES_PAGE_FEATURE_SUK_FUJI_CHAPTER_HISTORY_PARA_1",           "富士山是一座活火山，是日本最高的山峰。它高约12000英尺，常被云层包围，从东京可以看到它那著名的雪顶轮廓。这座雄伟的高山是无数艺术作品的主题——诗歌、歌曲、照片、雕塑和绘画。富士山也是日本的“三圣山”之一，与泰特山和哈古山并列。富士山也是全日本引以为豪的美丽象征。"),
+    ("zh_Hans_CN",  "LOC_FEATURE_SUK_FUJI_XP2_DESCRIPTION",                                      "占一个单元格无法逾越的自然奇观。以火山形式出现，并为相邻单元格所建造的区域额外提供两点对应类型的 [ICON_GreatPerson] 伟人点数，若区域位于火山土上则改为额外提供4点 [ICON_GreatPerson] 伟人点数。喷发时提供少量产出，有极高几率造成建筑与区域的损坏。不常喷发，但总处于活跃状态。"),
+    ("zh_Hans_CN",  "LOC_RANDOM_EVENT_FUJI_GENTLE_NAME",                                         "温和性喷发"),
+    ("zh_Hans_CN",  "LOC_RANDOM_EVENT_FUJI_GENTLE_DESCRIPTION",                                  "樱花丛中，人们忧心忡忡。而艺术家们则看着富士山上冉冉升起的烟柱，陶醉在诗情画意中。"),
+    ("zh_Hans_CN",  "LOC_RANDOM_EVENT_FUJI_CATASTROPHIC_NAME",                                   "灾难性喷发"),
+    ("zh_Hans_CN",  "LOC_RANDOM_EVENT_FUJI_CATASTROPHIC_DESCRIPTION",                            "诗人们描写着今天发生的恐怖事件，因为火山碎屑流吞噬了曾经平静的富士山周围的一切。"),
+    -- 大蓝洞
+    ("zh_Hans_CN",  "LOC_FEATURE_SUK_GREATBLUEHOLE_NAME",                                        "大蓝洞"),
+    ("zh_Hans_CN",  "LOC_FEATURE_SUK_GREATBLUEHOLE_DESCRIPTION",                                 "在礁石地貌上占一个单元格的自然奇观。产出+3 [ICON_Food] 食物、+2 [ICON_Production] 生产力、+2 [ICON_Gold] 金币。四格范围内所有文明的渔船产出+1 [ICON_Culture] 文化值。"),
+    ("zh_Hans_CN",  "LOC_FEATURE_SUK_GREATBLUEHOLE_QUOTE",                                       "自出生伊始，人类就将万有引力之沉重负于肩上，缚于大地。然而，仅需潜入海面之下，我们即可获得自由。[NEWLINE]——雅克-伊夫·库斯托"),
+    ("zh_Hans_CN",  "LOC_PEDIA_FEATURES_PAGE_FEATURE_SUK_GREATBLUEHOLE_CHAPTER_HISTORY_PARA_1",  "在距伯利兹外海大约100公里（62英里）的地方坐落着一处灯塔礁，一片闪烁着绿松石、青色、孔雀蓝和海蓝色等鲜艳色彩的小环礁。然而，在这一小片浅洋里，藏着一个深蓝色的巨大圆圈。它大约300米（1000英尺）宽，125米（400英尺）深，因此特征而被称为大蓝洞。它曾被著名水肺专家雅克·库斯托（Jacques Cousteau）赞誉为世界十大潜水宝地之一而负盛名。1971年，他驾驶他的船艇“卡利普索”号，潜入洞中绘制了深度图。探险队的此次调查，证实了该洞源于典型的喀斯特石灰岩地层，在至少四次海平面上升之前便已形成。尽管大蓝洞被列于潜水宝地清单中，但人们应意识到它并非适合所有潜水爱好者——一个先决条件便是潜水员要有超过24小时的深潜记录。而且，在此处潜水并无五彩斑斓的视觉体验。相反，潜水者将面对有着极为罕见的钟乳石的幽暗洞穴。"),
+    -- 火山口
+    ("zh_Hans_CN",  "LOC_FEATURE_SUK_NGORONGORO_CRATER_NAME",                                    "恩戈罗恩戈罗火山口"),
+    ("zh_Hans_CN",  "LOC_FEATURE_SUK_NGORONGORO_CRATER_DESCRIPTION",                             "占3个单元格的自然奇观。产出+2 [ICON_Food] 食物、+2 [ICON_Science] 科技值。相邻魅力在“迷人的”及以上的单元格，在未被改良时可产出+1 [ICON_Gold] 金币、+1 [ICON_Science] 科技值；被改良后则改为产出+1 [ICON_Culture] 文化值。"),
+    ("zh_Hans_CN",  "LOC_FEATURE_SUK_NGORONGORO_CRATER_QUOTE",                                   "没人敢说自己能永远定居于某处；只有群山，能岿然不动，安于此地。[NEWLINE]——马赛谚语"),
+    ("zh_Hans_CN",  "LOC_PEDIA_FEATURES_PAGE_FEATURE_SUK_NGORONGORO_CRATER_CHAPTER_HISTORY_PARA_1",  "恩戈罗恩戈罗火山口是世界上最大的完整火山口，风景优美，野生动物群集，是地球上最伟大的自然奇观之一。火山口及其周围地区生活着大约25000只大型动物，栖息着一系列濒危物种，如黑犀牛、非洲野狗、非洲金猫和近500种鸟类。[NEWLINE]它是地球上最大的动物迁徙地之一：每年有超过100万头角马、7.2万匹斑马、3万只汤普森瞪羚和格兰特瞪羚迁入或迁离火山口。[NEWLINE]火山口是面积更大的恩戈罗恩戈罗自然保护区的一部分。该保护区于1959年建立，是一片多用地区域，野生动物在此与实行传统牲畜放牧的半游牧马赛牧民共存。"),
+    -- 洞里萨湖
+    ("zh_Hans_CN",  "LOC_FEATURE_SUK_TONLESAP_NAME",                                             "洞里萨湖"),
+    ("zh_Hans_CN",  "LOC_FEATURE_SUK_TONLESAP_DESCRIPTION",                                      "在湖泊地形上占两个单元格的自然奇观。可作为淡水源，并产出+3 [ICON_Food] 食物、+2 [ICON_Production] 生产力。经过该奇观相邻单元格的建造者额外+1 [ICON_Charges] 劳动力。"),
+    ("zh_Hans_CN",  "LOC_FEATURE_SUK_TONLESAP_QUOTE",                                            "在一年当中有超过五个月的时间，柬埔寨的洞萨里湖都覆盖着一片广阔的土地；这段时期过后，湖水深度因大量蒸发而降低，但其宽度却几乎不变。[NEWLINE]——亨利·穆奥"),
+    ("zh_Hans_CN",  "LOC_PEDIA_FEATURES_PAGE_FEATURE_SUK_TONLESAP_CHAPTER_HISTORY_PARA_1",       "洞里萨湖（Tonlé Sap）俗称“大湖”（Great Lake），泛指季节性泛滥的淡水湖、洞里萨湖和一条连接该湖和湄公河的附属河流，即长120公里的洞里萨河。它们构成了复杂的水文系统的核心部分，坐落于12876平方公里的柬埔寨漫滩上，覆盖着有天然农耕环境的地块，而这些地块每年会从湄公河中补充水分与沉淀。[NEWLINE]作为世界上种类最多样、物产最丰富的生态系统之一，该地区一直是柬埔寨粮食供应的中心。事实证明，它能够在很大程度上维持高棉文明——世界历史上最大的工业时代前综合群居文明。[NEWLINE]作为东南亚最大的淡水湖，洞里萨湖拥有种类繁多、相互联系、生物多样性高的生态区，于1997年被联合国教科文组织列为生态保护区。");
