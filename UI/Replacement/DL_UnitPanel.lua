7ae0987469745f8fde1845f0c1aaf276ddd44942
PhantomJ_M <576130114@qq.com>
2021-11-09 20:52:43 +0800

    Rika_Support

--- UI/Replacement/DL_UnitPanel.lua
+++ UI/Replacement/DL_UnitPanel.lua
@@ -19,6 +19,7 @@ end
 include("Common");
 include("FarmsOnFreshHills_Common");
 include("HD_UnitCommandDefs");
+include "Hanyuu_CommandDefs";               --  Mod Furudu Rika Support
 
 Utils = ExposedMembers.DLHD.Utils;
 GreatPersonUtils = ExposedMembers.DLHD.GreatPersonUtils;
@@ -83,6 +84,48 @@ function GetUnitActionsTable(pUnit : object)
             end
         end
     --end
+
+    --  Mod Furudu Rika Support
+    if m_HanyuuUnitCommands then
+        for sCommandKey, pCommandTable in pairs(m_HanyuuUnitCommands) do
+		
+		--if UnitManager.CanStartCommand(pUnit, UnitCommandTypes.EXECUTE_SCRIPT) then
+			local bVisible : boolean = true;
+			if (pCommandTable.IsVisible ~= nil) then
+				bVisible = pCommandTable.IsVisible(pUnit);
+			end
+			if (bVisible) then
+
+				if (pCommandTable.CanUse ~= nil and pCommandTable.CanUse(pUnit) == true) then
+
+					local bIsDisabled : boolean = false;
+					if (pCommandTable.IsDisabled ~= nil) then
+						bIsDisabled = pCommandTable.IsDisabled(pUnit);
+					end
+			
+					local sToolTipString : string = pCommandTable.ToolTipString or "Undefined Unit Command";
+
+					local pCallback : ifunction = function()
+						local pSelectedUnit = UI.GetHeadSelectedUnit();
+						if (pSelectedUnit == nil) then
+							return;
+						end
+
+						local tParameters = {};
+						tParameters[UnitCommandTypes.PARAM_NAME] = pCommandTable.EventName or "";
+						UnitManager.RequestCommand(pSelectedUnit, UnitCommandTypes.EXECUTE_SCRIPT, tParameters);
+					end
+
+					if (bIsDisabled and pCommandTable.DisabledToolTipString ~= nil) then
+						sToolTipString = sToolTipString .. "[NEWLINE][NEWLINE]" .. pCommandTable.DisabledToolTipString;
+					end
+
+					AddActionToTable(pBaseActionsTable, pCommandTable, bIsDisabled, sToolTipString, UnitCommandTypes.EXECUTE_SCRIPT, pCallback);
+				end
+			end
+		--end
+	    end
+    end
 
     return pBaseActionsTable;
 end
