debb886695df3d69fbc34e3b028ee3586d0a6f0c
DeepLogic <>
2021-11-18 17:20:50 +0800

    [v1.1] builder speedup policy change to 50%

--- DLCSupport/DL_Aztec.sql
+++ DLCSupport/DL_Aztec.sql
@@ -23,3 +23,27 @@ insert or replace into TypeTags (Type, Tag)
     select UnitType,    'CLASS_CAPTURE_WORKER' from Units where PromotionClass = 'PROMOTION_CLASS_MELEE';
 
 update Units set Cost = 60 where UnitType = 'UNIT_AZTEC_EAGLE_WARRIOR';
+
+
+-- Wonder
+-- Huey Lake +1 Gold
+update Buildings set PrereqTech = 'TECH_ENGINEERING' where BuildingType = 'BUILDING_HUEY_TEOCALLI';
+
+insert or replace into BuildingModifiers
+	(BuildingType,							ModifierId)
+values
+	('BUILDING_HUEY_TEOCALLI',				'HUEY_LAKE_GOLD');
+
+insert or replace into Modifiers
+	(ModifierId, 							ModifierType,									SubjectRequirementSetId)
+values
+	('HUEY_LAKE_GOLD',						'MODIFIER_ALL_CITIES_ATTACH_MODIFIER',			'FOODHUEY_PLAYER_REQUIREMENTS'),		
+	('HUEY_LAKE_GOLD_MODIFIER',				'MODIFIER_CITY_PLOT_YIELDS_ADJUST_PLOT_YIELD',	'FOODHUEY_PLOT_IS_LAKE_REQUIREMENTS');		
+
+insert or replace into ModifierArguments
+	(ModifierId,							Name,			Value)
+values
+	('HUEY_LAKE_GOLD',						'ModifierId',	'HUEY_LAKE_GOLD_MODIFIER'),		
+	('HUEY_LAKE_GOLD_MODIFIER',				'YieldType',	'YIELD_GOLD'),		
+	('HUEY_LAKE_GOLD_MODIFIER',				'Amount',		1);
+-- update ModifierArguments set Value = 2 where ModifierId = 'HUEY_LAKE_FOOD_MODIFIER' and Name = 'Amount';
