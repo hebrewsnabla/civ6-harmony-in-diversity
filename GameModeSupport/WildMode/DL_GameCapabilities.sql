12d9c28a9ea42aaa76f65c4efc8af766efe7002e
DeepLogic <>
2021-11-20 12:49:13 +0800

    [dl] wild mode

---
+++ GameModeSupport/WildMode/DL_GameCapabilities.sql
@@ -0,0 +1,6 @@
+-------------------------------------
+--          Game Capabilities      --
+-------------------------------------
+
+insert or replace into Types (Type, Kind) values ('CAPABILITY_WILD',  'KIND_CAPABILITY');
+insert or replace into GameCapabilities (GameCapability) values ('CAPABILITY_WILD');
