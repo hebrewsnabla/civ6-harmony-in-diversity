3626570ae1d9e89ef761a9d57250c292f8860498
YT <774288220@qq.com>
2021-11-30 17:07:38 +0800

    耶稣会文本纠错+狂野模式版本changelog

--- GameModeSupport/WildMode/HD_Texts.sql
+++ GameModeSupport/WildMode/HD_Texts.sql
@@ -14,7 +14,7 @@
 insert or replace into LocalizedText
     (Language,      Tag,                                                     Text)
 values
-    ("zh_Hans_CN",  "LOC_MOMENT_CATEGORY_SCIENTIFIC_BONUS_GOLDEN_AGE",      "“自由探索”黄金时代：[NEWLINE][ICON_CivicBoosted]鼓舞额外提供10%的科技开销。城市每拥有1处特色区域，则+2 [ICON_Science] 科技值。港口提供等同于金币的科技。"),
+    ("zh_Hans_CN",  "LOC_MOMENT_CATEGORY_SCIENTIFIC_BONUS_GOLDEN_AGE",      "“自由探索”黄金时代：[NEWLINE][ICON_TechBoosted]尤里卡额外提供10%的科技开销。城市每拥有1处特色区域，则+2 [ICON_Science] 科技值。港口提供等同于金币的科技。"),
     ("zh_Hans_CN",  "LOC_DISTRICT_CULTURE_MBANZA",                          "+2 [ICON_Culture] 文化值来自相邻姆班赞。"),
     ("zh_Hans_CN",  "LOC_DISTRICT_GOLD_MBANZA",                             "+2 [ICON_Gold] 金币来自相邻姆班赞。"),
     ("zh_Hans_CN",  "LOC_DISTRICT_MBANZA_HD_DESCRIPTION",                   "刚果特色区域，只能建在雨林或树林里。替代社区，可更早建造，且建造费用更低。无视魅力值，固定提供+5 [ICON_Housing] 住房、+3 [ICON_Food] 食物和+5 [ICON_Gold] 金币。为相邻地块+1魅力，为剧院商业提供大加成。允许使用 [ICON_Faith] 信仰值购买开拓者和建造者。"),
@@ -28,7 +28,7 @@ values
     ("zh_Hans_CN",  "LOC_BELIEF_PILGRIMAGE_DL_DESCRIPTION",                 "每个信奉该宗教的城市+6 [ICON_Faith] 信仰。"),
     ("zh_Hans_CN",  "LOC_BELIEF_ZEN_MEDITATION_DL_DESCRIPTION",             "若圣地相邻山脉，+1 [ICON_Amenities] 宜居度；相邻森林，+1 [ICON_Amenities] 宜居度；相邻河流，+1 [ICON_housing] 住房，+2 [ICON_culture] 文化。市中心也获得相同加成。"),
     ("zh_Hans_CN",  "LOC_BELIEF_MESSIAH_DL_DESCRIPTION",                    "购买圣地区域内建筑的费用降低20%且可以用 [ICON_Faith] 信仰值购买。每个圣地区域内的建筑提供+8% [ICON_Faith] 信仰值。"),
-    ("zh_Hans_CN",  "LOC_BELIEF_JESUIT_EDUCATION_DL_DESCRIPTION",           "可以使用 [ICON_Faith] 信仰值购买学院区域内的建筑。每人口提供0.2 [ICON_Science] 科技值，若城市有神社每人口额外提供0.3 [ICON_Science] 科技值，有寺庙则变为0.5每人口额外提供0.3 [ICON_Science] 科技值。");
+    ("zh_Hans_CN",  "LOC_BELIEF_JESUIT_EDUCATION_DL_DESCRIPTION",           "可以使用 [ICON_Faith] 信仰值购买学院区域内的建筑。每人口提供0.2 [ICON_Science] 科技值，若城市有神社每人口提供0.5 [ICON_Science] 科技值，有寺庙则每人口提供1 [ICON_Science] 科技值。");
 
 -------------------------------------
 --     狂野模式 版本V0.2    --
