4eeccb7299ac48e737682be5938040aa30415081
YT <774288220@qq.com>
2021-12-19 19:11:49 +0800

    文本漏改补全

--- GameModeSupport/WildMode/HD_Texts.sql
+++ GameModeSupport/WildMode/HD_Texts.sql
@@ -14,21 +14,21 @@
 insert or replace into LocalizedText
     (Language,      Tag,                                                     Text)
 values
-    ("zh_Hans_CN",  "LOC_MOMENT_CATEGORY_SCIENTIFIC_BONUS_GOLDEN_AGE",      "“自由探索”黄金时代：[NEWLINE][ICON_TechBoosted]尤里卡额外提供10%的科技开销。城市每拥有1处特色区域，则+2 [ICON_Science] 科技值。港口提供等同于金币的科技。"),
-    ("zh_Hans_CN",  "LOC_DISTRICT_CULTURE_MBANZA",                          "+2 [ICON_Culture] 文化值来自相邻姆班赞。"),
-    ("zh_Hans_CN",  "LOC_DISTRICT_GOLD_MBANZA",                             "+2 [ICON_Gold] 金币来自相邻姆班赞。"),
-    ("zh_Hans_CN",  "LOC_DISTRICT_MBANZA_HD_DESCRIPTION",                   "刚果特色区域，只能建在雨林或树林里。替代社区，可更早建造，且建造费用更低。无视魅力值，固定提供+5 [ICON_Housing] 住房、+3 [ICON_Food] 食物和+5 [ICON_Gold] 金币。为相邻地块+1魅力，为剧院商业提供大加成。允许使用 [ICON_Faith] 信仰值购买开拓者和建造者。"),
-    ("zh_Hans_CN",  "LOC_TRAIT_CIVILIZATION_NKISI_DESCRIPTION",             "所有陆地单位无视森林和雨林对移动力的减益。所有巨作除了通常的文化值，还会从每个肖像，风景，宗教，遗物，文物以及雕塑艺术巨作中获得+3 [ICON_Food] 食物、+3 [ICON_Production] 生产力、+6 [ICON_Gold] 金币和+3 [ICON_Faith] 信仰值，书提供+2 [ICON_Food] 食物、+2 [ICON_Production] 生产力、+3 [ICON_Gold] 金币和+2 [ICON_Faith] 信仰值。获得额外100%大作家、大艺术家、大音乐家和大商人点数。宫殿有5个巨作槽位。"),
-    ("zh_Hans_CN",  "LOC_TRAIT_LEADER_RELIGIOUS_CONVERT_DESCRIPTION",       "无法建造圣地、获得大预言家，或创建宗教。从已成为多数刚果城市信仰的宗教中获得信仰值。每次完成一个姆班赞或剧院广场时获得一位（城市主流宗教的）使徒。剧院提供等同于相邻文化数值的信仰。"),
+    -- ("zh_Hans_CN",  "LOC_MOMENT_CATEGORY_SCIENTIFIC_BONUS_GOLDEN_AGE",      "“自由探索”黄金时代：[NEWLINE][ICON_TechBoosted]尤里卡额外提供10%的科技开销。城市每拥有1处特色区域，则+2 [ICON_Science] 科技值。港口提供等同于金币的科技。"),
+    -- ("zh_Hans_CN",  "LOC_DISTRICT_CULTURE_MBANZA",                          "+2 [ICON_Culture] 文化值来自相邻姆班赞。"),
+    -- ("zh_Hans_CN",  "LOC_DISTRICT_GOLD_MBANZA",                             "+2 [ICON_Gold] 金币来自相邻姆班赞。"),
+    -- ("zh_Hans_CN",  "LOC_DISTRICT_MBANZA_HD_DESCRIPTION",                   "刚果特色区域，只能建在雨林或树林里。替代社区，可更早建造，且建造费用更低。无视魅力值，固定提供+5 [ICON_Housing] 住房、+3 [ICON_Food] 食物和+5 [ICON_Gold] 金币。为相邻地块+1魅力，为剧院商业提供大加成。允许使用 [ICON_Faith] 信仰值购买开拓者和建造者。"),
+    -- ("zh_Hans_CN",  "LOC_TRAIT_CIVILIZATION_NKISI_DESCRIPTION",             "所有陆地单位无视森林和雨林对移动力的减益。所有巨作除了通常的文化值，还会从每个肖像，风景，宗教，遗物，文物以及雕塑艺术巨作中获得+3 [ICON_Food] 食物、+3 [ICON_Production] 生产力、+6 [ICON_Gold] 金币和+3 [ICON_Faith] 信仰值，书提供+2 [ICON_Food] 食物、+2 [ICON_Production] 生产力、+3 [ICON_Gold] 金币和+2 [ICON_Faith] 信仰值。获得额外100%大作家、大艺术家、大音乐家和大商人点数。宫殿有5个巨作槽位。"),
+    -- ("zh_Hans_CN",  "LOC_TRAIT_LEADER_RELIGIOUS_CONVERT_DESCRIPTION",       "无法建造圣地、获得大预言家，或创建宗教。从已成为多数刚果城市信仰的宗教中获得信仰值。每次完成一个姆班赞或剧院广场时获得一位（城市主流宗教的）使徒。剧院提供等同于相邻文化数值的信仰。"),
     ("zh_Hans_CN",  "LOC_BELIEF_DAR_E_MEHR_DL_DESCRIPTION",                 "允许建造拜火神庙（+2 [ICON_Faith] 信仰值；建造或上次修复后的每个时代均额外+2 [ICON_FAITH] 信仰值；完成后，获得一个传教士和一个武僧）。 "),
     ("zh_Hans_CN",  "LOC_BELIEF_SYNAGOGUE_DL_DESCRIPTION",                  "允许建造犹太教堂（+8 [ICON_Faith] 信仰值；本城+15% [ICON_FAITH] 信仰值）。"),
     ("zh_Hans_CN",  "LOC_BUILDING_SYNAGOGUE_DL_DESCRIPTION",                "本城+15% [ICON_FAITH] 信仰。"),
     ("zh_Hans_CN",  "LOC_BELIEF_TITHE_DL_DESCRIPTION",                      "该宗教每有1个信徒则+2 [ICON_Gold] 金币。"),
     ("zh_Hans_CN",  "LOC_BELIEF_RELIGIOUS_UNITY_DL_DESCRIPTION",            "该宗教每有1个信徒则+1 [ICON_Faith] 信仰。"),
     ("zh_Hans_CN",  "LOC_BELIEF_PILGRIMAGE_DL_DESCRIPTION",                 "每个信奉该宗教的城市+6 [ICON_Faith] 信仰。"),
-    ("zh_Hans_CN",  "LOC_BELIEF_ZEN_MEDITATION_DL_DESCRIPTION",             "若圣地相邻山脉，+1 [ICON_Amenities] 宜居度；相邻森林，+1 [ICON_Amenities] 宜居度；相邻河流，+1 [ICON_housing] 住房，+1 [ICON_culture] 文化。市中心也获得相同加成。"),
-    ("zh_Hans_CN",  "LOC_BELIEF_MESSIAH_DL_DESCRIPTION",                    "购买圣地区域内建筑的费用降低20%且可以用 [ICON_Faith] 信仰值购买。每个圣地区域内的建筑提供+8% [ICON_Faith] 信仰值。"),
-    ("zh_Hans_CN",  "LOC_BELIEF_JESUIT_EDUCATION_DL_DESCRIPTION",           "可以使用 [ICON_Faith] 信仰值购买学院区域内的建筑。每人口提供0.2 [ICON_Science] 科技值，若城市有神社每人口提供0.5 [ICON_Science] 科技值，有寺庙则每人口提供1 [ICON_Science] 科技值。");
+    ("zh_Hans_CN",  "LOC_BELIEF_ZEN_MEDITATION_DL_DESCRIPTION",             "若圣地相邻山脉，+1 [ICON_Amenities] 宜居度；相邻森林，+1 [ICON_Amenities] 宜居度；相邻河流，+1 [ICON_housing] 住房，+1 [ICON_culture] 文化。市中心也获得相同加成。");
+    -- ("zh_Hans_CN",  "LOC_BELIEF_MESSIAH_DL_DESCRIPTION",                    "购买圣地区域内建筑的费用降低20%且可以用 [ICON_Faith] 信仰值购买。每个圣地区域内的建筑提供+8% [ICON_Faith] 信仰值。"),
+    -- ("zh_Hans_CN",  "LOC_BELIEF_JESUIT_EDUCATION_DL_DESCRIPTION",           "可以使用 [ICON_Faith] 信仰值购买学院区域内的建筑。每人口提供0.2 [ICON_Science] 科技值，若城市有神社每人口提供0.5 [ICON_Science] 科技值，有寺庙则每人口提供1 [ICON_Science] 科技值。");
 
 -------------------------------------
 --     狂野模式 版本V0.2    --
@@ -57,12 +57,12 @@ values
     -- ("zh_Hans_CN",  "LOC_CIVILIZATION_RAPA_NUI_BONUS",                      "建造者可以建造摩艾石像改良设施。[NEWLINE][NEWLINE]摩艾石像提供+3 [ICON_Culture] 文化值。每2个相邻的摩艾石像+1 [ICON_Culture] 文化值。在火山土单元格之上或与其相邻时，+2 [ICON_Culture] 文化值。随着市政与科技树的发展，可获得额外的 [ICON_Culture] 文化值与 [ICON_Tourism] 旅游业绩。只可建在相邻湖泊或近海的平原、平原丘陵、草原、草原丘陵或火山土单元格之上。可以修在资源上并正确改良资源"),
     -- ("zh_Hans_CN",  "LOC_LEADER_TRAIT_MOHENJO_DARO_DESCRIPTION",            "为您的所有城市+1 [ICON_Housing] 住房。无论您的城市是否相邻淡水，水源提供的基础 [ICON_Housing] 住房均与相邻淡水时相同。拥有剧院的城市+1 [ICON_Amenities] 宜居度,+3 [ICON_FOOD] 食物"),
     -- ("zh_Hans_CN",  "LOC_CIVILIZATION_MOHENJO_DARO_BONUS",                  "为您的所有城市+1 [ICON_Housing] 住房。无论您的城市是否相邻淡水，水源提供的基础 [ICON_Housing] 住房均与相邻淡水时相同。拥有剧院的城市+1 [ICON_Amenities] 宜居度,+3 [ICON_FOOD] 食物"),
-    ("zh_Hans_CN",  "LOC_LEADER_TRAIT_VILNIUS_DESCRIPTION",                 "剧院广场区域的相邻加成提升50%，解锁人文主义、歌剧与芭蕾和文化遗产市政后分别额外提升50%的剧院广场相邻加成。"),
-    ("zh_Hans_CN",  "LOC_CIVILIZATION_VILNIUS_BONUS_XP1",                   "剧院广场区域的相邻加成提升50%，解锁人文主义、歌剧与芭蕾和文化遗产市政后分别额外提升50%的剧院广场相邻加成。"),
+    -- ("zh_Hans_CN",  "LOC_LEADER_TRAIT_VILNIUS_DESCRIPTION",                 "剧院广场区域的相邻加成提升50%，解锁人文主义、歌剧与芭蕾和文化遗产市政后分别额外提升50%的剧院广场相邻加成。"),
+    -- ("zh_Hans_CN",  "LOC_CIVILIZATION_VILNIUS_BONUS_XP1",                   "剧院广场区域的相邻加成提升50%，解锁人文主义、歌剧与芭蕾和文化遗产市政后分别额外提升50%的剧院广场相邻加成。"),
     -- ("zh_Hans_CN",  "LOC_LEADER_TRAIT_YEREVAN_DESCRIPTION",                 "你的使徒单位可以选择任何可用的升级，宗教单位+1 [ICON_Movement] 移动力"),
     -- ("zh_Hans_CN",  "LOC_CIVILIZATION_YEREVAN_CITY_BONUS",                 "你的使徒单位可以选择任何可用的升级，宗教单位+1 [ICON_Movement] 移动力"),
-    ("zh_Hans_CN",  "LOC_LEADER_TRAIT_VATICAN_CITY_DESCRIPTION",            "每本 [ICON_GreatWork_WRITING] 著作巨作+2 [ICON_Culture] 文化值和+2 [ICON_FAITH] 信仰值。每件 [ICON_GreatWork_Sculpture] 雕塑、 [ICON_GreatWork_Portrait] 肖像、 [ICON_GreatWork_Landscape] 风景和 [ICON_GreatWork_Religious] 宗教艺术巨作+3 [ICON_Culture] 文化值和+3 [ICON_FAITH] 信仰值。每件 [ICON_GreatWork_RELIC] 遗物+3 [ICON_Culture] 文化值。"),
-    ("zh_Hans_CN",  "LOC_CIVILIZATION_VATICAN_CITY_BONUS",                  "每本 [ICON_GreatWork_WRITING] 著作巨作+2 [ICON_Culture] 文化值和+2 [ICON_FAITH] 信仰值。每件 [ICON_GreatWork_Sculpture] 雕塑、 [ICON_GreatWork_Portrait] 肖像、 [ICON_GreatWork_Landscape] 风景和 [ICON_GreatWork_Religious] 宗教艺术巨作+3 [ICON_Culture] 文化值和+3 [ICON_FAITH] 信仰值。每件 [ICON_GreatWork_RELIC] 遗物+3 [ICON_Culture] 文化值。");
+    -- ("zh_Hans_CN",  "LOC_LEADER_TRAIT_VATICAN_CITY_DESCRIPTION",            "每本 [ICON_GreatWork_WRITING] 著作巨作+2 [ICON_Culture] 文化值和+2 [ICON_FAITH] 信仰值。每件 [ICON_GreatWork_Sculpture] 雕塑、 [ICON_GreatWork_Portrait] 肖像、 [ICON_GreatWork_Landscape] 风景和 [ICON_GreatWork_Religious] 宗教艺术巨作+3 [ICON_Culture] 文化值和+3 [ICON_FAITH] 信仰值。每件 [ICON_GreatWork_RELIC] 遗物+3 [ICON_Culture] 文化值。"),
+    -- ("zh_Hans_CN",  "LOC_CIVILIZATION_VATICAN_CITY_BONUS",                  "每本 [ICON_GreatWork_WRITING] 著作巨作+2 [ICON_Culture] 文化值和+2 [ICON_FAITH] 信仰值。每件 [ICON_GreatWork_Sculpture] 雕塑、 [ICON_GreatWork_Portrait] 肖像、 [ICON_GreatWork_Landscape] 风景和 [ICON_GreatWork_Religious] 宗教艺术巨作+3 [ICON_Culture] 文化值和+3 [ICON_FAITH] 信仰值。每件 [ICON_GreatWork_RELIC] 遗物+3 [ICON_Culture] 文化值。");
 
     -- ("zh_Hans_CN",  "LOC_LEADER_TRAIT_ARMAGH_EXPANSION2_DESCRIPTION",       "建造者现在能够建造修道院改良设施。[NEWLINE][NEWLINE]修道院提供+2 [ICON_Faith] 信仰值。此单元格上的友方宗教单位生命值恢复+15（前提是其当前回合未发动攻击）。每1个相邻的区域使其+2 [ICON_FAITH] 信仰值。解锁“归正会”市政后每1个相邻的区域使其+3 [ICON_FAITH] 信仰值。提供+1 [ICON_HOUSING] 住房。解锁“殖民主义”市政后获得额外1点 [ICON_HOUSING] 住房。每个城只能建造一个。"),
     -- ("zh_Hans_CN",  "LOC_CIVILIZATION_ARMAGH_BONUS_XP2",                    "建造者现在能够建造修道院改良设施。[NEWLINE][NEWLINE]修道院提供+2 [ICON_Faith] 信仰值。此单元格上的友方宗教单位生命值恢复+15（前提是其当前回合未发动攻击）。每1个相邻的区域使其+2 [ICON_FAITH] 信仰值。解锁“归正会”市政后每1个相邻的区域使其+3 [ICON_FAITH] 信仰值。提供+1 [ICON_HOUSING] 住房。解锁“殖民主义”市政后获得额外1点 [ICON_HOUSING] 住房。每个城只能建造一个。"),
