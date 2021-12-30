107c86d5ecdded6970f3f7d8a49408b5f040d714
zhushou <582372326@qq.com>
2021-12-30 22:56:28 +0800

    [test] fix text of song of Siren for suk  klep

--- ModSupport/SukOceans/HD_Texts.sql
+++ ModSupport/SukOceans/HD_Texts.sql
@@ -149,9 +149,11 @@ VALUES
 insert or replace into EnglishText
     (Tag,                                                                                   Text)
 values
-    ("LOC_TECH_CELESTIAL_NAVIGATION_HD_DESCRIPTION",                                        "Allows harvesting of Kelp Forest and resources improved by Fishing Boats. If Ship Building is not researched, also allows all land units to embark.");
+    ("LOC_TECH_CELESTIAL_NAVIGATION_HD_DESCRIPTION",                                        "Allows harvesting of Kelp Forest and resources improved by Fishing Boats. If Ship Building is not researched, also allows all land units to embark."),
+    ("LOC_BELIEF_HD_SONG_OF_SIREN_DL_DESCRIPTION",                                          "Campus receive an additional Major adjacency bonus for being adjacent to Reef or Klep. +2 [ICON_Production] Production from Reef and Klep.");
 
 insert or replace into LocalizedText
     (Language,      Tag,                                                                    Text)
 values
-    ("zh_Hans_CN",  "LOC_TECH_CELESTIAL_NAVIGATION_HD_DESCRIPTION",                         "允许收获海藻森林和由渔船改良的资源。若未掌握造船术，也允许所有陆地单位上船。");
+    ("zh_Hans_CN",  "LOC_TECH_CELESTIAL_NAVIGATION_HD_DESCRIPTION",                         "允许收获海藻森林和由渔船改良的资源。若未掌握造船术，也允许所有陆地单位上船。"),
+    ("zh_Hans_CN",  "LOC_BELIEF_HD_SONG_OF_SIREN_DL_DESCRIPTION",                           "礁石和海藻森林为学院提供额外大相邻加成，礁石和海藻森林单元格+2 [ICON_Production] 生产力。");
