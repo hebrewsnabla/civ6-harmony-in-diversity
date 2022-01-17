303b08702e4bdbab3620d4232f6a5cf546b34309
YT <774288220@qq.com>
2022-01-17 20:13:49 +0800

    pvpuu更新（未完工）

--- GameModeSupport/MultiPlayerMode/DL_MultiPlayer.sql
+++ GameModeSupport/MultiPlayerMode/DL_MultiPlayer.sql
@@ -71,7 +71,31 @@ update Units set BaseMoves = 3 where UnitType = 'UNIT_SUMERIAN_PHALANX';
 --诸葛连弩
 update Units set RangedCombat = 40 where UnitType = 'UNIT_CHINESE_CHOKONU';
 --罗马弩炮
--- update Units set Bombard = 35 where UnitType = 'UNIT_ROMAN_ONAGER';
+update Units set Bombard = 35 where UnitType = 'UNIT_ROMAN_ONAGER';
+insert or replace into Types
+    (Type,                                              Kind)
+values
+    ('ABILITY_COMBAT_STRENGTH_BONUS_TO_LAND_COMBAT',    'KIND_ABILITY');
+insert or replace into TypeTags
+    (Type,                                              Tag)
+values
+    ('ABILITY_COMBAT_STRENGTH_BONUS_TO_LAND_COMBAT',    'CLASS_ROMAN_ONAGER');
+insert or replace into UnitAbilities
+    (UnitAbilityType,                                   Name,                                                       Description,                                                        Permanent)
+values
+    ('ABILITY_COMBAT_STRENGTH_BONUS_TO_LAND_COMBAT',    'LOC_ABILITY_COMBAT_STRENGTH_BONUS_TO_LAND_COMBAT_NAME',    'LOC_ABILITY_COMBAT_STRENGTH_BONUS_TO_LAND_COMBAT_DESCRIPTION',     1);
+insert or replace into UnitAbilityModifiers
+    (UnitAbilityType,                                   ModifierId)
+values
+    ('ABILITY_COMBAT_STRENGTH_BONUS_TO_LAND_COMBAT',    'MOD_ABILITY_COMBAT_STRENGTH_BONUS_TO_LAND_COMBAT');
+insert or replace into Modifiers
+    (ModifierId,                                        ModifierType,                           SubjectRequirementSetId)
+values
+    ('MOD_ABILITY_COMBAT_STRENGTH_BONUS_TO_LAND_COMBAT','MODIFIER_UNIT_ADJUST_COMBAT_STRENGTH','GRAPE_SHOT_REQUIREMENTS');
+insert or replace into ModifierArguments
+    (ModifierId,                                        Name,                           Value)
+values
+    ('MOD_ABILITY_COMBAT_STRENGTH_BONUS_TO_LAND_COMBAT','Amount',                       17);
 --矿工军团
 update ModifierArguments set Value = 0 where ModifierId = 'DIGGER_BONUS_ON_COAST' AND Name='Amount';
 --长生军
@@ -98,7 +122,20 @@ update Units set Combat = 35 where UnitType = 'UNIT_AZTEC_JAGUAR';
 update Units set Cost = 60 where UnitType = 'UNIT_AZTEC_EAGLE_WARRIOR';
 update Units set Cost = 60 where UnitType = 'UNIT_AZTEC_JAGUAR';
 --苏丹亲兵
-
+delete from UnitReplaces where CivUniqueUnitType = 'UNIT_SULEIMAN_JANISSARY';
+--挪威牧师
+update Units set BaseMoves = 4 where UnitType = 'UNIT_NORWEGIAN_ULFHEDNAR';
+--高棉战象
+update Units set Bombard = 45 where UnitType = 'UNIT_KHMER_DOMREY';
+insert or replace into TypeTags
+    (Type,                                              Tag)
+values
+    ('ABILITY_COMBAT_STRENGTH_BONUS_TO_LAND_COMBAT',    'CLASS_DOMREY'),
+    ('UNIT_KHMER_DOMREY',                               'CLASS_DOMREY');
+insert or replace into Tags
+    (Tag,               Vocabulary)
+values
+    ('CLASS_DOMREY',    'ABILITY_CLASS');
 
 
 
@@ -114,7 +151,6 @@ update Units set Cost = 60 where UnitType = 'UNIT_AZTEC_JAGUAR';
 --斯基泰
 delete from TraitModifiers where ModifierId = 'TRAIT_TECH_ANIMAL_HUSBANDRY' or ModifierId = 'TRAIT_PASTURE_PRODUCTION';
 delete from TraitModifiers where ModifierId = 'TRAIT_EXTRA_SCYTHIAN_AMAZON';
-
 --祖鲁
 delete from TraitModifiers where ModifierId = 'TRAIT_LAND_CORPS_COMBAT_STRENGTH' or ModifierId = 'TRAIT_LAND_ARMIES_COMBAT_STRENGTH';
 update Units set PrereqTech = 'TECH_MACHINERY' where UnitType = 'UNIT_ZULU_ASSEGAI';
@@ -185,12 +221,62 @@ UPDATE ModifierArguments SET Value= 2 WHERE ModifierId='TRAIT_JOAO_TRADE_ROUTE_O
 -------------------------------------
 --           市政&科技             --
 -------------------------------------
---远程占领区域加力
+--远程
+--占领区域加力
 delete from CivicModifiers where ModifierId = "HD_RANGED_GARRISON_BONUS";
 insert or replace into TechnologyModifiers
     (TechnologyType,                               ModifierId)
 values
     ('TECH_MACHINERY',                             'HD_RANGED_GARRISON_BONUS');
+--境内进攻5力
+delete from TechnologyModifiers where ModifierId = "HD_RANGED_HILLS_STRENGTH";
+insert or replace into CivicModifiers
+    (CivicType,                               ModifierId)
+values
+    ('CIVIC_HUMANISM',                        'HD_RANGED_ATTACK_BONUS');
+insert or replace into Modifiers
+	(ModifierId,							ModifierType)
+values
+	('HD_RANGED_ATTACK_BONUS',				'MODIFIER_PLAYER_UNITS_GRANT_ABILITY');
+insert or replace into ModifierArguments
+	(ModifierId,					Name,						Value)
+values
+	('HD_RANGED_ATTACK_BONUS',		'AbilityType',				'ABILITY_HD_RANGED_ATTACK_BONUS');
+insert or replace into Types
+    (Type,                                                      Kind)
+values
+    ('ABILITY_HD_RANGED_ATTACK_BONUS',                          'KIND_ABILITY');
+insert or replace into TypeTags
+    (Type,                                                      Tag)
+values
+    ('ABILITY_HD_RANGED_ATTACK_BONUS',                          'CLASS_RANGED');
+insert or replace into UnitAbilities 
+    (UnitAbilityType,                                           Name,   Description,                                                            Inactive) 
+values
+    ('ABILITY_HD_RANGED_ATTACK_BONUS',                          NULL,   'LOC_ABILITY_HD_RANGED_ATTACK_BONUS_DESCRIPTION',                       1);
+insert or replace into UnitAbilityModifiers
+    (UnitAbilityType,                                           ModifierId)
+values
+    ('ABILITY_HD_RANGED_ATTACK_BONUS',                        'ABILITY_HD_RANGED_ATTACK_BONUS_MODIFIER');
+insert or replace into Modifiers
+    (ModifierId,                                         ModifierType,                           SubjectRequirementSetId)
+values
+    ('ABILITY_HD_RANGED_ATTACK_BONUS_MODIFIER',        'MODIFIER_UNIT_ADJUST_COMBAT_STRENGTH',  'HD_RANGED_ATTACK_BONUS_REQUIREMENTS');
+insert or replace into ModifierArguments
+	(ModifierId,					                Name,						Value)
+values
+	('ABILITY_HD_RANGED_ATTACK_BONUS_MODIFIER',		'Amount',				    5);
+insert or ignore into RequirementSets
+	(RequirementSetId,												RequirementSetType)
+values
+	('HD_RANGED_ATTACK_BONUS_REQUIREMENTS',							'REQUIREMENTSET_TEST_ALL');
+insert or ignore into RequirementSetRequirements
+	(RequirementSetId,												RequirementId)
+values
+	('HD_RANGED_ATTACK_BONUS_REQUIREMENTS',							'PLAYER_IS_ATTACKER_REQUIREMENTS'),
+	('HD_RANGED_ATTACK_BONUS_REQUIREMENTS',							'UNIT_IN_OWNER_TERRITORY_REQUIREMENT');
+
+
 --抗骑兵
 delete from  TechnologyModifiers where ModifierId = "HD_ANTIC_IGNORE_DAMAGED_STRENGTH_REDUCTION";
 insert or replace into TechnologyModifiers
