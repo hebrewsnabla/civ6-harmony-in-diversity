935ffb2bbb4f462090e0da486685c3fc7126a245
FiveYear <2052327812@qq.com>
2022-03-14 22:48:37 +0800

    translation of mod civilizations, part 1

---
+++ ModSupport/ModCivilizations/Text.sql
@@ -0,0 +1,45 @@
+--------------------------------------------------------------------------------
+-- Language: en_US
+-- insert or replace into EnglishText
+--     (Tag,                                                                       Text)
+-- values
+--     ();
+--------------------------------------------------------------------------------
+-- Language: zh_Hans_CN
+insert or replace into LocalizedText
+    (Language,      Tag,                                                                    Text)
+values
+    ("zh_Hans_CN",  "LOC_CIVILIZATION_GEDEMO_ZIMBABWE_NAME", "津巴布韦"),
+    ("zh_Hans_CN",  "LOC_CIVILIZATION_GEDEMO_ZIMBABWE_DESCRIPTION", "津巴布韦"),
+    ("zh_Hans_CN",  "LOC_LEADER_GEDEMO_MUTOTA", "恩雅茨姆巴·姆托塔"),
+    ("zh_Hans_CN",  "LOC_TRAIT_CIVILIZATION_GEDEMO_ZIMBABWE_CIV_TRAIT_NAME", "穆塔帕崛起"),
+    ("zh_Hans_CN",  "LOC_TRAIT_CIVILIZATION_GEDEMO_ZIMBABWE_CIV_TRAIT_DESCRIPTION", "帝国初期和封建主义各解锁一个额外的商路名额。每级城墙+1大商点。单位从河边开始回合时，回合内+10战斗力。"),
+    ("zh_Hans_CN",  "LOC_TRAIT_LEADER_GEDEMO_MUTOTA_LTRAIT_NAME", "跨越废土之旅"),
+    ("zh_Hans_CN",  "LOC_TRAIT_LEADER_GEDEMO_MUTOTA_LTRAIT_DESCRIPTION", "国际商路为出发地+2忠诚度。国际商路+1文化+1金币，从目的地每个种植园和营地获得+2文化+1金币。"),
+    ("zh_Hans_CN",  "LOC_UNIT_GEDEMO_ROZWI_NAME", "瓦杰罗"),
+    ("zh_Hans_CN",  "LOC_UNIT_GEDEMO_ROZWI_DESCRIPTION", "津巴布韦远古时代特色单位，取代枪兵，战斗力更强，解锁更早"),
+    ("zh_Hans_CN",  "LOC_IMPROVEMENT_GEDEMO_DZIMBABWE_NAME", "津巴布韦石城"),
+    ("zh_Hans_CN",  "LOC_IMPROVEMENT_GEDEMO_DZIMBABWE_DESCRIPTION", "砌砖解锁的津巴布韦特色改良设施。+2粮食+2金币+1住房。城市中第一座津巴布韦石城为所在城市+1宜居度，研究“自然历史”以后，第二座津巴布韦石城+1位所在城市+1宜居度。周围单元格+1金币。战斗单位在津巴布韦石城单元格时+4战斗力。不能与另一个津巴布韦石城相邻。"),
+    ("zh_Hans_CN",  "LOC_TRAIT_LEADER_GEDEMO_CHARLEMAGNE1_NAME", "王家使者"),
+    ("zh_Hans_CN",  "LOC_TRAIT_LEADER_GEDEMO_CHARLEMAGNE1_DESCRIPTION", " 圣地相邻也获得金币。研究马镫以后，获得特色单位圣骑士。"),
+    ("zh_Hans_CN",  "LOC_UNIT_GEDEMO_PALADIN_NAME", "圣骑士"),
+    ("zh_Hans_CN",  "LOC_UNIT_GEDEMO_PALADIN_DESCRIPTION", "相邻敌方单位-5战斗力，可以叠加。"),
+    ("zh_Hans_CN",  "LOC_TRAIT_LEADER_GEDEMO_THEODORA_NAME", "秘史"),
+    ("zh_Hans_CN",  "LOC_TRAIT_LEADER_GEDEMO_THEODORA_DESCRIPTION", "信仰宗教的城市+1宜居度，+4食物。可以训练蛮族雇佣兵（野蛮弓骑手）。可以建造特色改良忏悔室。"),
+    ("zh_Hans_CN",  "LOC_IMPROVEMENT_GEDEMO_METANOIA_NAME", "忏悔室"),
+    ("zh_Hans_CN",  "LOC_IMPROVEMENT_GEDEMO_METANOIA_DESCRIPTION", "必须建在沿海陆地上。不能于另一个忏悔室相邻。+1住房，+2信仰，+2金币，+5忠诚度。若相邻圣地，则再加1信仰"),
+    ("zh_Hans_CN",  "LOC_TRAIT_LEADER_GEDEMO_CATHERINE2_NAME", "西伯利亚的财富"),
+    ("zh_Hans_CN",  "LOC_TRAIT_LEADER_GEDEMO_CATHERINE2_DESCRIPTION", "战略资源+1 生产力。正确改良的马，铁，铀每回合产出2相应资源。当您在您首都的贸易路线范围内建立或征服一座市中心与冻土或雪地相邻的城市时，自动获得一条通往首都的道路。"),
+    ("zh_Hans_CN",  "LOC_LEADER_LEU_PACAL_NAME", "卡盾之盛"),
+    ("zh_Hans_CN",  "LOC_LEADER_LEU_PACAL_DESCRIPTION", "农场在生产奇观的城市中+1生产力。奇观完成时每个相邻的农场，获得科学和信仰的爆发；这种爆发在黄金时代会增加，在新时代开始时也会重复。"),
+    ("zh_Hans_CN",  "LOC_CIVILIZATION_MER_HITTITES_NAME", "赫梯"),
+    ("zh_Hans_CN",  "LOC_CIVILIZATION_MER_HITTITES_DESCRIPTION", "赫梯"),
+    ("zh_Hans_CN",  "LOC_LEADER_MER_SUPPILULIUMA_NAME", "苏庇路里乌玛一世"),
+    ("zh_Hans_CN",  "LOC_TRAIT_CIVILIZATION_MER_SUN_AND_STORM_NAME", "烈阳与暴风"),
+    ("zh_Hans_CN",  "LOC_TRAIT_CIVILIZATION_MER_SUN_AND_STORM_DESCRIPTION", "成为宗主的每一个城邦类型为所有城市+10%的对应产出。来自宗主城邦和同盟的外交支持+100%。流星坑也会给予一个部落村落的奖励。"),
+    ("zh_Hans_CN",  "LOC_TRAIT_LEADER_MER_BOND_AND_OATH_NAME", "纽带和誓言"),
+    ("zh_Hans_CN",  "LOC_TRAIT_LEADER_MER_BOND_AND_OATH_DESCRIPTION", "占领敌方城市后，所有城市+10%生产力和信仰，持续10回合。被占领的城市会释放更多的忠诚压力。法典解锁报复性战争和保护城邦战争。"),
+    ("zh_Hans_CN",  "LOC_UNIT_MER_ANSUKURRA_NAME", "赫梯战车"),
+    ("zh_Hans_CN",  "LOC_UNIT_MER_ANSUKURRA_DESCRIPTION", "取代重型战车的赫梯远古时代特色单位。战斗力更高而更贵。无视丘陵带来的移动力减益。双倍支援加成。"),
+    ("zh_Hans_CN",  "LOC_BUILDING_MER_ROYAL_ARCHIVE_NAME", "皇家档案馆"),
+    ("zh_Hans_CN",  "LOC_BUILDING_MER_ROYAL_ARCHIVE_DESCRIPTION", "取代图书馆的赫梯远古时代特色建筑：在首都建造时提供+1点影响力。外国文明中的每个代表团或大使馆提供+1科技值。(上限为4)建造成本较高，维护费较高。");
