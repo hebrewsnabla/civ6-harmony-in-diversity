d8e65e4e58fc8b79b1d72cebe53242067ab47cd8
FiveYear <2052327812@qq.com>
2022-03-20 20:59:28 +0800

    简中修缮翻译搬运

--- ModSupport/ModCivilizations/CopyFiles.py
+++ ModSupport/ModCivilizations/CopyFiles.py
@@ -3,10 +3,13 @@ import os,shutil
 def mycopyfile(path, path1, path2):
     file_path = path + path1
     fpath, fname = os.path.split(file_path)
+    # print(path2)
+    # print(file_path,path2+fname)
     shutil.copy(file_path, path2 + fname)
 ModCiv_path = 'C:/Users/f/Documents/My Games/Sid Meier\'s Civilization VI/Mods/civ6-harmony-in-diversity/ModSupport/ModCivilizations'
-with open(ModCiv_path + '/TextToLocalizeList.txt','r') as file1, open(ModCiv_path + '/ModCivId.txt','w') as file2:
-    path2 = 'C:/Users/f/Documents/My Games/Sid Meier\'s Civilization VI/Mods/Text/'
+
+with open(ModCiv_path + '/FileOthersTrans.txt','r') as file1, open(ModCiv_path + '/ModCivId.txt','w') as file2:
+    path2 = 'C:/Users/f/Documents/My Games/Sid Meier\'s Civilization VI/Mods/files/'
     data = file1.readlines()
     data.pop(0)
     workshop_path = data.pop(0)
@@ -17,11 +20,12 @@ with open(ModCiv_path + '/TextToLocalizeList.txt','r') as file1, open(ModCiv_pat
     for line in data:
         if line[0].isdigit():
             mod_tag = line[3:-1]
-            print(mod_tag, file=file2)
+            print(mod_tag)
         else:
             path1 = line[13:-1]
-            mod_id = line[13:13+10]
+            mod_id = line[13+1:13+10+1] # Cation!
             xml_list.append(path1)
             # print(workshop_path+path1)
-            print(mod_id[1:], file=file2)
-            mycopyfile(workshop_path,path1,path2 + mod_id + '_')
+            print(mod_id)
+            # print(path1)
+            mycopyfile(workshop_path, path1, path2 + mod_id + '_')
