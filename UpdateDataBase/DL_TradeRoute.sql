7dc1883e33c284938b902ed974c94f8995c54792
FiveYear <2052327812@qq.com>
2022-05-06 04:05:31 +0800

    issue fix in trade route

--- UpdateDataBase/DL_TradeRoute.sql
+++ UpdateDataBase/DL_TradeRoute.sql
@@ -4,12 +4,12 @@
 
 update District_TradeRouteYields set YieldChangeAsInternationalDestination = 3 where DistrictType = 'DISTRICT_GOVERNMENT' and YieldType = 'YIELD_GOLD';
 
--- Commercial hub for domestic traderoute yields (1 food + 1 production)
-insert or replace into District_TradeRouteYields
- 	(DistrictType,				YieldType,			YieldChangeAsDomesticDestination,	YieldChangeAsInternationalDestination)
-values
- 	('DISTRICT_COMMERCIAL_HUB',	'YIELD_FOOD',		1,									0),
- 	('DISTRICT_SUGUBA',			'YIELD_FOOD',		1,									0);
+-- Commercial hub for domestic traderoute yields (1 food + 1 production) remove 1 food
+-- insert or replace into District_TradeRouteYields
+--  	(DistrictType,				YieldType,			YieldChangeAsDomesticDestination,	YieldChangeAsInternationalDestination)
+-- values
+--  	('DISTRICT_COMMERCIAL_HUB',	'YIELD_FOOD',		1,									0),
+--  	('DISTRICT_SUGUBA',			'YIELD_FOOD',		1,									0);
 
 insert or replace into DistrictModifiers (DistrictType, ModifierId)
 select DistrictType, 'HD_ONE_GOLD_FROM_INCOMING_FOREIGN_ROUTES' from District_TradeRouteYields
